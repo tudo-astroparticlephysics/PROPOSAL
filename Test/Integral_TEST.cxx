2c1c4afe7c21490fb00c8dc5f29350c6376380e8
Tomasz Fuchs <tomasz.fuchs@tu-dortmund.de>
2013-03-19 14:14:20 +0000

    Changed some names and added more examples in the unit test Integral_Test. Awesome output with ctest -V

--- Test/Integral_TEST.cxx
+++ Test/Integral_TEST.cxx
@@ -1,14 +1,38 @@
 #include "gtest/gtest.h"
 #include "PROPOSAL/Integral.h"
+#include "math.h"
 
 double Testfkt(double r){
   return r*r;
 }
 
+double Testexp(double r){
+    return exp(r);
+}
 
-TEST(FooTest, DoesXyz) {
+bool relErr(double Is, double HasToBe, double RelError){
+    return fabs((Is - HasToBe)/HasToBe)<RelError;
+}
+
+TEST(IntegralValue , Zero_to_Three_of_xx ) {
     Integral* Int = new Integral();
     EXPECT_EQ(Int->IntegrateClosed(0,3,Testfkt),9);
+}
+
+TEST(IntegralValue, EqualBorders) {
+    Integral* Int = new Integral();
+    EXPECT_EQ(Int->IntegrateClosed(3,3,Testfkt),0);
+}
+
+TEST(IntegralValue, SmallError) {
+    Integral* Int = new Integral();
+    EXPECT_TRUE(  relErr(   Int->IntegrateClosed(0,3,Testexp),exp(3)-1
+                            ,1.e-6) //Later GetPrecision();
+                );
+}
+
+TEST(IntegralValue, HasToFail) {
+    EXPECT_TRUE(false);
 }
 
 int main(int argc, char **argv) {
