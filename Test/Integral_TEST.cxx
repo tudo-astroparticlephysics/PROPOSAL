b5866e903b47489e3365765726a0f0a611ef873d
Tomasz Fuchs <tomasz.fuchs@tu-dortmund.de>
2013-03-19 15:52:44 +0000

    Added some more simple tests for the Integral class. Added one time comsuming function.

--- Test/Integral_TEST.cxx
+++ Test/Integral_TEST.cxx
@@ -17,18 +17,51 @@ bool relErr(double Is, double HasToBe, double RelError){
 TEST(IntegralValue , Zero_to_Three_of_xx ) {
     Integral* Int = new Integral();
     EXPECT_EQ(Int->IntegrateClosed(0,3,Testfkt),9);
+    delete Int;
 }
 
 TEST(IntegralValue, EqualBorders) {
     Integral* Int = new Integral();
+
     EXPECT_EQ(Int->IntegrateClosed(3,3,Testfkt),0);
+
+    delete Int;
 }
 
 TEST(IntegralValue, SmallError) {
     Integral* Int = new Integral();
+
     EXPECT_TRUE(  relErr(   Int->IntegrateClosed(0,3,Testexp),exp(3)-1
                             ,1.e-6) //Later GetPrecision();
                 );
+
+    delete Int;
+}
+
+TEST(IntegralValue, FloatEqual) {
+    Integral* Int = new Integral();
+
+    //Last 4 digits can differ. relError<1E-4
+    ASSERT_FLOAT_EQ(Int->IntegrateClosed(0,3,Testexp),exp(3)-1);
+
+    delete Int;
+}
+
+TEST(IntegralValue, MultiplePrecisions) {
+    double xmin=0,xmax=3;
+    double  ExactIntegral=exp(3)-1;
+    double  CalcIntegral=0;
+
+    double precision = 1E-5;
+    for(double precision = 1E-5; precision>1E-16;precision/=10){
+
+        Integral* Int = new Integral(5,20,precision);
+        for(int i=1000;i>0;i--)CalcIntegral = i* Int->IntegrateClosed(xmin,xmax,Testexp);
+
+        ASSERT_NEAR(CalcIntegral,ExactIntegral, ExactIntegral*precision);
+
+        delete Int;
+    }
 }
 
 TEST(IntegralValue, HasToFail) {
