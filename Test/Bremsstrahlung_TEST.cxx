962045b1ecf706af686a392c33351993ecc892f5
Jan-Hendrik Köhne <jan-hendrik.koehne@tu-dortmund.de>
2013-03-20 12:28:15 +0000

    adding bremsstrahlung test

---
+++ Test/Bremsstrahlung_TEST.cxx
@@ -0,0 +1,54 @@
+#include "gtest/gtest.h"
+#include "PROPOSAL/Bremsstrahlung.h"
+#include "PROPOSAL/CrossSections.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+TEST(Bremsstrahlung , Test_of_dEdx ) {
+
+    ifstream in;
+    in.open("bin/Brems_dEdx.txt");
+
+    char firstLine[256];
+    in.getline(firstLine,256);
+
+    double energy;
+    double dEdx;
+    double ecut;
+    double vcut;
+    string med;
+    string particleName;
+    bool lpm;
+    int para;
+
+    CrossSections *brems = new Bremsstrahlung();
+
+    while(in.good())
+    {
+        in>>para>>ecut>>vcut>>lpm>>energy>>med>>particleName>>dEdx;
+        //if(lpm!=0)continue;
+        Medium *medium = new Medium(med,1.);
+        brems->GetEnergyCutSettings()->SetEcut(ecut);
+        brems->GetEnergyCutSettings()->SetVcut(vcut);
+        brems->SetMedium(medium);
+        brems->SetParametrization(para);
+        brems->EnableLpmEffect(lpm);
+        Particle *particle = new Particle(particleName,1.,1.,1,.20,20,1e5,10);
+        particle->SetEnergy(energy);
+        brems->SetParticle(particle);
+        ASSERT_FLOAT_EQ(brems->CalculatedEdx(), dEdx);
+        delete medium;
+        delete particle;
+
+
+
+    }
+    delete brems;
+}
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
