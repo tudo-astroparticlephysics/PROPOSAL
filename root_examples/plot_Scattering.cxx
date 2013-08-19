315681fc700f31fa35e74b91996bfbc3af856efe
Tomasz Fuchs <tomasz.fuchs@tu-dortmund.de>
2013-08-19 13:29:13 +0000

    variable number of particles depended of energy in the plot_Scattering.cxx script.

--- root_examples/plot_Scattering.cxx
+++ root_examples/plot_Scattering.cxx
@@ -4,6 +4,9 @@
 #include "TFile.h"
 #include <iostream>
 #include <sstream>
+#include <stdio.h>
+#include <string>
+
 
 using namespace std;
 
@@ -24,7 +27,7 @@ int main()
     vector<TH1D*> tmp_hist_angle_vec;
 
 
-    int number_of_particles =   1e3;
+    int number_of_particles =   3e6;
 
     double distance;
     double deviation;
@@ -33,6 +36,7 @@ int main()
     double Emin=1e3;
     double Emax=1e12;
     double multiplier = 10;
+
 
     for(double energy = Emin; energy <= Emax ; energy *= multiplier)
     {
@@ -47,21 +51,27 @@ int main()
 
         hist_name   << "Scattering_dev_"    <<energy;
         hist_name2  << "Scattering_angle_"  <<energy;
-        hist_title  << "dev. of mouns with E="<<energy<< "and range(";
-        hist_title2 << "angle of mouns with E="<<energy<< "and range(";
+        hist_title  << "dev. of mouns with E="<<energy<< " and range(";
+        hist_title2 << "angle of mouns with E="<<energy<< " and range(";
 
         distance    = 0;
         deviation   = 0;
         angle       = 0;
 
-        TH1D *tmp_hist_dev = new TH1D(hist_name.str().c_str(),"dd",64,1,1000);
-        TH1D *tmp_hist_angle = new TH1D(hist_name2.str().c_str(),"ss",64,1e-8,1);
-        cout<<tmp_hist_dev<<endl;
-        cout<<tmp_hist_angle<<endl;
+        TH1D *tmp_hist_dev = new TH1D(hist_name.str().c_str(),"dd",80,-2,6);
+        TH1D *tmp_hist_angle = new TH1D(hist_name2.str().c_str(),"ss",50,-5,0);
+
+        cout << "Energy: " << energy << endl;
+        bool cross = false;
         for(int i =0 ;i< number_of_particles; i++)
         {
-            if(i%(number_of_particles/10)==0)cout<<"Progress: " <<100.*i/number_of_particles<<"%"<<endl;
-
+            if(i%(number_of_particles/10)==0)
+            {
+                if(cross)cout<< "+";
+                if(!cross)cout<< "#";
+                cross = !cross;
+                fflush(stdout);
+            }
             Particle * prtcl = new Particle("mu",0,0,0,0,0,energy,0);
 
             prpgtr->Propagate(prtcl);
@@ -70,10 +80,8 @@ int main()
             deviation   = sqrt(prtcl->GetX()*prtcl->GetX()+prtcl->GetY()*prtcl->GetY());
             angle       = prtcl->GetTheta();
 
-            tmp_hist_dev->Fill(deviation);
-            tmp_hist_angle->Fill(angle);
-
-            //delete prtcl;
+            tmp_hist_dev->Fill(log10(deviation));
+            tmp_hist_angle->Fill(log10(angle));
         }
         hist_title  <<   distance/number_of_particles   <<")";
         hist_title2 <<   distance/number_of_particles   <<")";
@@ -83,14 +91,17 @@ int main()
         tmp_hist_dev->SetTitle(hist_title.str().c_str());
         tmp_hist_dev_vec.push_back(tmp_hist_dev);
 
-        //delete tmp_hist_dev;
 
         tmp_hist_angle->GetXaxis()->SetTitle("angle [deg]");
         tmp_hist_angle->GetYaxis()->SetTitle("#");
         tmp_hist_angle->SetTitle(hist_title2.str().c_str());
         tmp_hist_angle_vec.push_back(tmp_hist_angle);
-        //delete tmp_hist_angle;
 
+        cout << endl;
+        if(log10(energy)>=4 && (number_of_particles>=1e3))
+        {
+            number_of_particles/=10;
+        }
     }
 
     TFile *file = new TFile("Scattering_distribution.root","RECREATE");
