2f634e2d57d4f591697765512c5ef1ec6f888426
mario <mario.dunsch@tu-dortmund.de>
2017-04-10 14:06:27 +0200

    Some renameings.

--- resources/examples/standalone/test.py
+++ resources/examples/standalone/test.py
@@ -1,41 +1,47 @@
 import pyPROPOSAL
 
+med = pyPROPOSAL.Medium("ice")
+E = pyPROPOSAL.EnergyCutSettings(500, 1)
 ptype = pyPROPOSAL.ParticleType.MuMinus
-mu = pyPROPOSAL.Particle(ptype)
-mu.energy = 1e10
 
-p = pyPROPOSAL.Propagator(
-    config="resources/configuration",
-    particle=mu
-)
+p = pyPROPOSAL.Propagator(med, E, ptype, "")
+p.particle.energy = 1e12
 
-mu_length = list()
-n_daughters = list()
+d = p.propagate()
+print(len(d))
 
-for i in range(1000):
-    p.reset_particle()
-    d = p.propagate()
+# a = pr.cross_sections
 
-    mu_length.append(mu.propagated_distance)
-    # mu_length.append(p.GetParticle().GetPropagatedDistance())
-    n_daughters.append(len(d))
+# p = pyPROPOSAL.Propagator(med, E, ptype, "resources/tables")
+# print(len(p.current_collection.cross_sections))
 
+# mu_length = list()
+# n_daughters = list()
+#
+# for i in range(10):
+#     p.reset_particle()
+#     p.particle.energy = 1e10
+#
+#     d = p.propagate()
+#
+#     mu_length.append(p.particle.propagated_distance)
+#     n_daughters.append(len(d))
 
-try:
-    import matplotlib as mpl
-    mpl.use('Agg')
-    import pylab
-
-    pylab.figure()
-    pylab.title("Mu Lengths")
-    pylab.hist(mu_length, histtype="step", log=True, bins=100)
-    pylab.xlabel(r'$l_{\mu}(\rm{m})$')
-    pylab.savefig('MuonLenghts.pdf')
-
-    pylab.figure()
-    pylab.title("N Daughters")
-    pylab.hist(n_daughters, histtype="step", log=True, bins=100)
-    pylab.xlabel('N')
-    pylab.savefig('Daughters.pdf')
-except ImportError :
-    print("pylab not installed.  no plots for you.")
+# try:
+#     import matplotlib as mpl
+#     mpl.use('Agg')
+#     import pylab
+#
+#     pylab.figure()
+#     pylab.title("Mu Lengths")
+#     pylab.hist(mu_length, histtype="step", log=True, bins=100)
+#     pylab.xlabel(r'$l_{\mu}(\rm{m})$')
+#     pylab.savefig('MuonLenghts.pdf')
+#
+#     pylab.figure()
+#     pylab.title("N Daughters")
+#     pylab.hist(n_daughters, histtype="step", log=True, bins=100)
+#     pylab.xlabel('N')
+#     pylab.savefig('Daughters.pdf')
+# except ImportError :
+#     print("pylab not installed.  no plots for you.")
