63ef0274dd2a19d73cef16e3b81f9a4f9e1908b6
mario <mario.dunsch@tu-dortmund.de>
2017-04-28 13:54:33 +0200

    Add copy constructor to pybind.
    Also modified python examples further.

--- resources/examples/standalone/plot_track.py
+++ resources/examples/standalone/plot_track.py
@@ -1,150 +1,24 @@
-import pyPROPOSAL
-import numpy as np
-
-ptype = pyPROPOSAL.ParticleType.MuMinus
-mu = pyPROPOSAL.Particle(ptype)
-
-med = pyPROPOSAL.Medium("ice")
-
-cuts_inside = pyPROPOSAL.EnergyCutSettings(500, -1)
-cuts_infront = pyPROPOSAL.EnergyCutSettings(-1, 0.05)
-cuts_behind = pyPROPOSAL.EnergyCutSettings(-1, 0.05)
-
-prop = pyPROPOSAL.Propagator(med, cuts_inside, ptype, "../../resources/tables")
-mu = prop.particle
-
-mu.energy = 9e6
-mu.Z = -1e5
-
-mu_start_energy = mu.energy
-mu_start_pos = (mu.X, mu.Y, mu.Z)
-mu_start_phi = mu.phi
-mu_start_theta = mu.theta
-
-
-geo_detector = pyPROPOSAL.Geometry()
-geo_detector.init_cylinder(
-    x0=0,
-    y0=0,
-    z0=0,
-    radius=800,
-    inner_radius=0,
-    z=1600
-)
-
-geo_outside = pyPROPOSAL.Geometry()
-geo_outside.init_box(
-    x0=0,
-    y0=0,
-    z0=0,
-    x=500000,
-    y=500000,
-    z=500000
-)
-
-collection_detector = pyPROPOSAL.ProcessCollection(mu, med, cuts_inside)
-collection_detector.location = 1
-collection_detector.geometry = geo_detector
-collection_detector.enable_randomization = True
-
-collection_infront = pyPROPOSAL.ProcessCollection(mu, med, cuts_infront)
-collection_infront.location = 0
-collection_infront.geometry = geo_outside
-collection_infront.enable_randomization = True
-
-collection_behind = pyPROPOSAL.ProcessCollection(mu, med, cuts_behind)
-collection_behind.location = 2
-collection_behind.geometry = geo_outside
-collection_behind.enable_randomization = True
-
-prop.collections = [
-    collection_infront,
-    collection_detector,
-    collection_behind
-]
-
-prop.detector = geo_detector
-prop.apply_options()
-
-# print(prop.collections)
-
-secondarys = prop.propagate()
-
-# # =========================================================
-# # 	Plot
-# # =========================================================
-#
-# try:
-#     import matplotlib.pyplot as plt
-#     from mpl_toolkits.mplot3d import Axes3D
-# except ImportError:
-#     print("matplotlib not installed.  no plots for you.")
-#
-# try:
-#     import numpy as np
-# except ImportError:
-#     print("numpy not installed.  no plots for you.")
-#
-# tex_preamble = [
-#     r"\usepackage{amsmath}",
-#     r"\usepackage[utf8]{inputenc}",
-#     r"\usepackage[T1]{fontenc}",
-#     r"\usepackage{siunitx}",
-# ]
-#
-# font_size = 10
-#
-# params = {
-#     'backend': 'pdf',
-#     'font.family': 'serif',
-#     'font.size': 12,
-#     'text.usetex': True,
-#     'text.latex.preamble': tex_preamble,
-#     'axes.labelsize': font_size,
-#     'legend.numpoints': 1,
-#     'legend.shadow': False,
-#     'legend.fontsize': font_size,
-#     'xtick.labelsize': font_size,
-#     'ytick.labelsize': font_size,
-#     'axes.unicode_minus': True
-# }
-#
-# plt.rcParams.update(params)
-#
-#
-# fig = plt.figure()
-# ax = fig.add_subplot(111, projection='3d')
-#
-# # Cylinder
-# x = np.linspace(-1, 1, 100)
-# z = np.linspace(-2, 2, 100)
-# Xc, Zc = np.meshgrid(x, z)
-# Yc = np.sqrt(1-Xc**2)
-#
-# # Draw parameters
-# rstride = 20
-# cstride = 10
-# ax.plot_surface(Xc, Yc, Zc, alpha=0.2, rstride=rstride, cstride=cstride)
-# ax.plot_surface(Xc, -Yc, Zc, alpha=0.2, rstride=rstride, cstride=cstride)
-#
-# ax.set_xlabel("x / m")
-# ax.set_ylabel("y / m")
-# ax.set_zlabel("z / m")
-# plt.show()
-#
-# # fig.savefig("track.pdf")
 
 import math
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
 
-import mpl_toolkits.mplot3d.art3d as art3d
-import matplotlib.pyplot as plt
-import numpy as np
-from matplotlib.patches import Circle
+try:
+    import matplotlib.pyplot as plt
+    from mpl_toolkits.mplot3d import Axes3D
+except ImportError:
+    raise ImportError("Matplotlib not installed!")
+
+try:
+    import numpy as np
+except ImportError:
+    raise ImportError(
+        "Numpy not installed! Needed to calculate the detector cylinder"
+    )
+
+import pyPROPOSAL
 
 
 def plot_3D_cylinder(
+    ax,
     radius,
     height,
     elevation=0,
@@ -160,8 +34,6 @@ def plot_3D_cylinder(
 
     Y = np.sqrt(radius**2 - (X - x_center)**2) + y_center
 
-    # ax.plot_surface(X, Y, Z, linewidth=0, color=color, alpha=alpha)
-    # ax.plot_surface(X, (2*y_center-Y), Z, linewidth=0, color=color, alpha=alpha)
     ax.plot_wireframe(
         X,
         Y,
@@ -170,7 +42,7 @@ def plot_3D_cylinder(
         color=color,
         alpha=alpha,
         rcount=1,
-        ccount=1
+        ccount=10
     )
     ax.plot_wireframe(
         X,
@@ -180,19 +52,12 @@ def plot_3D_cylinder(
         color=color,
         alpha=alpha,
         rcount=1,
-        ccount=1
+        ccount=10
     )
 
     ax.w_xaxis.gridlines.set_lw(0.2)
     ax.w_yaxis.gridlines.set_lw(0.2)
     ax.w_zaxis.gridlines.set_lw(0.2)
-    # floor = Circle((x_center, y_center), radius, color=color, alpha=alpha)
-    # ax.add_patch(floor)
-    # art3d.pathpatch_2d_to_3d(floor, z=elevation, zdir="z")
-    #
-    # ceiling = Circle((x_center, y_center), radius, color=color, alpha=alpha)
-    # ax.add_patch(ceiling)
-    # art3d.pathpatch_2d_to_3d(ceiling, z=elevation+height, zdir="z")
 
     return ax
 
@@ -206,53 +71,161 @@ def sphere_to_cart(magnitude, phi, theta):
     return x, y, z
 
 
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
+def propagate():
+    """ Propagte muon in ice threw a cylindric detector
 
-ax = plot_3D_cylinder(
-    geo_detector.radius / 100,
-    geo_detector.z / 100,
-    elevation=-geo_detector.z / 2.0 / 100,
-    resolution=10,
-    color='k',
-    alpha=0.5,
-    x_center=geo_detector.x0 / 100,
-    y_center=geo_detector.y0 / 100
-)
+    Returns:
+        (Particle) Particle representing the start position
+        (Geometry) Geometry of the detector
+        (list)     List of secondarys particles represeint interactions
+    """
+    ptype = pyPROPOSAL.ParticleType.MuMinus
+    mu = pyPROPOSAL.Particle(ptype)
 
-mu_end = prop.particle
-ax.scatter(mu_start_pos[0]/100, mu_start_pos[1]/100, mu_start_pos[2]/100, c='r')
+    med = pyPROPOSAL.Medium("ice")
 
-x, y, z = sphere_to_cart(200, mu_start_phi, mu_start_theta)
-ax.quiver(mu_start_pos[0]/100, mu_start_pos[1]/100, mu_start_pos[2]/100, x, y, z)
-# ax.scatter(mu_end.X/100, mu_end.Y/100, mu_end.Z/100, c='r')
+    cuts_inside = pyPROPOSAL.EnergyCutSettings(500, -1)
+    cuts_infront = pyPROPOSAL.EnergyCutSettings(-1, 0.05)
+    cuts_behind = pyPROPOSAL.EnergyCutSettings(-1, 0.05)
 
-for interaction in secondarys:
-    if geo_detector.is_particle_inside(interaction):
-        color = 'b'
-        if interaction.type == pyPROPOSAL.ParticleType.Brems:
+    prop = pyPROPOSAL.Propagator(
+        med,
+        cuts_inside,
+        ptype,
+        "../../resources/tables"
+    )
+    mu = prop.particle
+
+    mu.energy = 9e6
+    mu.Z = -1e5
+
+    mu_start = pyPROPOSAL.Particle(mu)
+
+    geo_detector = pyPROPOSAL.Geometry()
+    geo_detector.init_cylinder(
+        x0=0,
+        y0=0,
+        z0=0,
+        radius=800,
+        inner_radius=0,
+        z=1600
+    )
+
+    geo_outside = pyPROPOSAL.Geometry()
+    geo_outside.init_box(
+        x0=0,
+        y0=0,
+        z0=0,
+        x=500000,
+        y=500000,
+        z=500000
+    )
+
+    collection_detector = pyPROPOSAL.ProcessCollection(mu, med, cuts_inside)
+    collection_detector.location = 1
+    collection_detector.geometry = geo_detector
+    collection_detector.enable_randomization = True
+
+    collection_infront = pyPROPOSAL.ProcessCollection(mu, med, cuts_infront)
+    collection_infront.location = 0
+    collection_infront.geometry = geo_outside
+    collection_infront.enable_randomization = True
+
+    collection_behind = pyPROPOSAL.ProcessCollection(mu, med, cuts_behind)
+    collection_behind.location = 2
+    collection_behind.geometry = geo_outside
+    collection_behind.enable_randomization = True
+
+    prop.collections = [
+        collection_infront,
+        collection_detector,
+        collection_behind
+    ]
+
+    prop.detector = geo_detector
+    prop.apply_options()
+
+    secondarys = prop.propagate()
+
+    return mu_start, geo_detector, secondarys
+
+
+def plot_track(mu_start, geo_detector, secondarys):
+    """ Create 3D plot of the track.
+
+    Args:
+        mu_start (Particle): particle representing the start position
+        geo_detector (Geometry): Geometry of the detector
+        secondarys (list): List of secondarys particles representing the
+                           interactions
+
+    Returns: None
+    """
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+
+    ax = plot_3D_cylinder(
+        ax,
+        geo_detector.radius / 100,
+        geo_detector.z / 100,
+        elevation=-geo_detector.z / 2.0 / 100,
+        resolution=10,
+        color='k',
+        alpha=0.5,
+        x_center=geo_detector.x0 / 100,
+        y_center=geo_detector.y0 / 100
+    )
+
+    ax.scatter(
+        mu_start.X/100,
+        mu_start.Y/100,
+        mu_start.Z/100,
+        c='r'
+    )
+
+    x, y, z = sphere_to_cart(200, mu_start.phi, mu_start.theta)
+
+    ax.quiver(
+        mu_start.X/100,
+        mu_start.Y/100,
+        mu_start.Z/100,
+        x,
+        y,
+        z
+    )
+
+    for interaction in secondarys:
+        if geo_detector.is_particle_inside(interaction):
             color = 'b'
-        elif interaction.type == pyPROPOSAL.ParticleType.EPair:
-            color = 'r'
-        elif interaction.type == pyPROPOSAL.ParticleType.NuclInt:
-            color = 'm'
-        elif interaction.type == pyPROPOSAL.ParticleType.DeltaE:
-            color = 'g'
-        else:
-            color = 'k'
+            if interaction.type == pyPROPOSAL.ParticleType.Brems:
+                color = 'b'
+            elif interaction.type == pyPROPOSAL.ParticleType.EPair:
+                color = 'r'
+            elif interaction.type == pyPROPOSAL.ParticleType.NuclInt:
+                color = 'm'
+            elif interaction.type == pyPROPOSAL.ParticleType.DeltaE:
+                color = 'g'
+            else:
+                color = 'k'
 
-        ax.scatter(
-            interaction.X/100,
-            interaction.Y/100,
-            interaction.Z/100,
-            c=color,
-            alpha=0.2,
-            s=interaction.energy/400
-        )
+            ax.scatter(
+                interaction.X/100,
+                interaction.Y/100,
+                interaction.Z/100,
+                c=color,
+                alpha=0.2,
+                s=interaction.energy/400
+            )
 
-ax.set_xlabel('x-axis')
-ax.set_ylabel('y-axis')
-ax.set_zlabel('z-axis')
+    ax.set_xlabel('x / m')
+    ax.set_ylabel('y / m')
+    ax.set_zlabel('z / m')
 
-plt.show()
-# fig.savefig('track.pdf')
+    plt.show()
+
+    return fig
+
+if __name__ == "__main__":
+
+    fig = plot_track(*propagate())
+    # fig.savefig('track.pdf')
