da0aaa493caabf513e07b6e9b85f16f70f25af42
mario <mario.dunsch@tu-dortmund.de>
2017-10-09 10:59:31 +0200

    added selfwritten erfinv
    Used to be able to profile.
    boost::erfinv crashes with valgrind

--- private/PROPOSAL/methods.cxx
+++ private/PROPOSAL/methods.cxx
@@ -41,6 +41,53 @@
 namespace PROPOSAL
 {
 
+float myErfInv2(float x)
+{
+   float tt1, tt2, lnx, sgn;
+   sgn = (x < 0) ? -1.0f : 1.0f;
+
+   x = (1 - x)*(1 + x);        // x = 1 - x*x;
+   lnx = logf(x);
+
+   tt1 = 2/(3.141592653589793*0.147) + 0.5f * lnx;
+   tt2 = 1/(0.147) * lnx;
+
+   return(sgn*sqrtf(-tt1 + sqrtf(tt1*tt1 - tt2)));
+}
+
+// double RationalApproximation(double t)
+// {
+//     // Abramowitz and Stegun formula 26.2.23.
+//     // The absolute value of the error should be less than 4.5 e-4.
+//     double c[] = {2.515517, 0.802853, 0.010328};
+//     double d[] = {1.432788, 0.189269, 0.001308};
+//     return t - ((c[2]*t + c[1])*t + c[0]) /
+//                (((d[2]*t + d[1])*t + d[0])*t + 1.0);
+// }
+//
+// double NormalCDFInverse(double p)
+// {
+//     if (p <= 0.0 || p >= 1.0)
+//     {
+//         std::stringstream os;
+//         os << "Invalid input argument (" << p
+//            << "); must be larger than 0 but less than 1.";
+//         throw std::invalid_argument( os.str() );
+//     }
+//
+//     // See article above for explanation of this section.
+//     if (p < 0.5)
+//     {
+//         // F^-1(p) = - G^-1(p)
+//         return -RationalApproximation( sqrt(-2.0*log(p)) );
+//     }
+//     else
+//     {
+//         // F^-1(p) = G^-1(1-p)
+//         return RationalApproximation( sqrt(-2.0*log(1-p)) );
+//     }
+// }
+
 bool FileExist(std::string path)
 {
     struct stat dummy_stat_return_val;
