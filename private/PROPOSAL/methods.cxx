5902720f40ba9c3050fd30fd36ea688ad575986a
mario <mario.dunsch@tu-dortmund.de>
2017-10-11 16:46:38 +0200

    Moved path code to own function

--- private/PROPOSAL/methods.cxx
+++ private/PROPOSAL/methods.cxx
@@ -47,20 +47,6 @@ float myErfInv2(float x)
    return(sgn*sqrtf(-tt1 + sqrtf(tt1*tt1 - tt2)));
 }
 
-bool FileExist(std::string path)
-{
-    struct stat dummy_stat_return_val;
-
-    if (stat(path.c_str(), &dummy_stat_return_val) != 0)
-    {
-        return false;
-    }
-    else
-    {
-        return true;
-    }
-}
-
 // round a given double to the closest int
 int RoundValue(double val)
 {
@@ -84,24 +70,24 @@ int RoundValue(double val)
     return valRound;
 }
 
-InvalidPath::InvalidPath(const char* pathname, const char* err)
-    : pathname_(pathname)
-    , msg_()
-{
-    msg_ = std::string(err) + ": " + pathname_;
-}
-
-InvalidPath::InvalidPath(const std::string& pathname, const char* err)
-    : pathname_(pathname)
-    , msg_()
-{
-    msg_ = std::string(err) + ": " + pathname_;
-}
-
-const char* InvalidPath::what() const throw()
-{
-    return msg_.c_str();
-}
+// InvalidPath::InvalidPath(const char* pathname, const char* err)
+//     : pathname_(pathname)
+//     , msg_()
+// {
+//     msg_ = std::string(err) + ": " + pathname_;
+// }
+//
+// InvalidPath::InvalidPath(const std::string& pathname, const char* err)
+//     : pathname_(pathname)
+//     , msg_()
+// {
+//     msg_ = std::string(err) + ": " + pathname_;
+// }
+//
+// const char* InvalidPath::what() const throw()
+// {
+//     return msg_.c_str();
+// }
 
 namespace Helper
 {
@@ -119,6 +105,33 @@ std::string Centered(int width, const std::string& str, char fill)
     int pad1 = diff / 2;
     int pad2 = diff - pad1;
     return std::string(pad1, fill) + str + std::string(pad2, fill);
+}
+
+// ------------------------------------------------------------------------- //
+std::string ResolvePath(const std::string& pathname)
+{
+    wordexp_t p;
+    // Use WRDE_UNDEF to consider undefined shell variables as error
+    int success = wordexp(pathname.c_str(), &p, WRDE_UNDEF );
+
+    if (success != 0)
+    {
+        log_warn("Invalid path given: %s", pathname.c_str());
+        return "";
+    }
+
+    char full_path[PATH_MAX];
+    char* resolved = realpath(*p.we_wordv, full_path);
+
+    wordfree( &p );
+
+    if (!resolved)
+    {
+        log_warn("Invalid path given: %s", full_path);
+        return "";
+    }
+
+    return std::string(resolved);
 }
 
 // ------------------------------------------------------------------------- //
@@ -149,23 +162,7 @@ void InitializeInterpolation(const std::string name,
     bool storing_failed =   false;
     bool reading_worked =   true;
 
-
-    // Expand environment variables in path and get full path
-    wordexp_t p;
-    int success = wordexp(interpolation_def.path_to_tables.c_str(), &p, 0 );
-
-    char full_path[PATH_MAX];
-    char* resolved = realpath(*p.we_wordv, full_path);
-
-    wordfree( &p );
-
-    if (!resolved)
-    {
-        throw InvalidPath(full_path, strerror(errno));
-    }
-
-    std::string pathname(resolved);
-
+    std::string pathname = ResolvePath(interpolation_def.path_to_tables);
     bool raw = interpolation_def.raw;
 
     // --------------------------------------------------------------------- //
@@ -216,10 +213,6 @@ void InitializeInterpolation(const std::string name,
                 //TODO(mario): read check Tue 2017/09/05
                 (*builder_it->second) = new Interpolant();
                 (*builder_it->second)->Load(input, raw);
-
-                // for (std::vector<Interpolant*>::iterator interpol_it = builder_it->second.begin(); interpol_it != builder_it->second.end(); ++interpol_it)
-                // {
-                // }
             }
 
             input.close();
@@ -266,7 +259,7 @@ void InitializeInterpolation(const std::string name,
 
     if(pathname.empty() || storing_failed)
     {
-        log_info("%s tables will be stored in memomy: %s", name.c_str(), filename.str().c_str());
+        log_info("%s tables will be stored in memomy!", name.c_str());
 
         for (InterpolantBuilderContainer::iterator builder_it = builder_container.begin(); builder_it != builder_container.end(); ++builder_it)
         {
