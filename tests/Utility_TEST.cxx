816fd0d764630f5ebe808414a9f9eb13516021a3
sudojan <jan.soedingrekso@tu-dortmund.de>
2017-10-26 17:28:37 +0200

    add a specification to stopping decay

--- tests/Utility_TEST.cxx
+++ tests/Utility_TEST.cxx
@@ -9,25 +9,40 @@ using namespace PROPOSAL;
 
 TEST(Comparison , Comparison_equal )
 {
-    Water water1(1.0);
-    Water water2(1.0);
-    EXPECT_TRUE(water1 == water2);
+    Water water(1.0);
+    EnergyCutSettings ecuts;
+    ParticleDef pDef(MuMinusDef::Get());
+    Utility::Definition utility_defs;
 
-    EnergyCutSettings ecuts1;
-    EnergyCutSettings ecuts2;
-    EXPECT_TRUE(ecuts1 == ecuts2);
+    Utility utils1(pDef, water, ecuts, utility_defs);
+    Utility utils2(pDef, water, ecuts, utility_defs);
+
+    EXPECT_TRUE(utils1==utils2);
+}
+
+TEST(Comparison , Comparison_not_equal )
+{
+    Water water1(1.0);
+    Water water2(0.9);
+
+    EnergyCutSettings ecuts1(500,0.05);
+    EnergyCutSettings ecuts2(200,0.01);
 
     ParticleDef pDef1(MuMinusDef::Get());
-    ParticleDef pDef2(MuMinusDef::Get());
-    EXPECT_TRUE(pDef1 == pDef2);
+    ParticleDef pDef2(TauMinusDef::Get());
 
     Utility::Definition utility_defs;
 
     Utility utils1(pDef1, water1, ecuts1, utility_defs);
-    Utility utils2(pDef2, water2, ecuts2, utility_defs);
+    Utility utils2(pDef2, water1, ecuts1, utility_defs);
+    Utility utils3(pDef1, water2, ecuts1, utility_defs);
+    Utility utils4(pDef1, water1, ecuts2, utility_defs);
 
-    EXPECT_TRUE(utils1==utils2);
+    EXPECT_TRUE(utils1!=utils2);
+    EXPECT_TRUE(utils1!=utils3);
+    EXPECT_TRUE(utils1!=utils4);
 }
+
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
