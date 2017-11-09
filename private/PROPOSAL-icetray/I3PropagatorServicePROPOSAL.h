f600f1923d3e4819fa26b230eec11e92960c3610
mario <mario.dunsch@tu-dortmund.de>
2017-11-09 17:52:02 +0100

    Merge

--- private/PROPOSAL-icetray/I3PropagatorServicePROPOSAL.h
+++ private/PROPOSAL-icetray/I3PropagatorServicePROPOSAL.h
@@ -10,21 +10,14 @@
 #include <boost/shared_ptr.hpp>
 #include <boost/utility.hpp>
 
-#include <icetray/I3Logging.h>
-#include <icetray/I3PointerTypedefs.h>
-#include <sim-services/I3PropagatorService.h>
+#include "icetray/I3Logging.h"
+#include "icetray/I3PointerTypedefs.h"
+#include "sim-services/I3PropagatorService.h"
 #include "simclasses/I3MMCTrack.h"
 
-#include "PROPOSAL/Propagator.h" //Tomasz
-// #include "PROPOSAL/PROPOSALParticle.h"
-
+#include "PROPOSAL/PROPOSAL.h"
 
 class I3Particle;
-
-  // class PROPOSALParticle;
-  // class Output;
-  // class Propagator; //Tomasz
-
 
 /**
  * @version $Id: I3PropagatorService.h 68823 2010-11-22 15:14:19Z dima $
@@ -39,84 +32,48 @@ namespace PROPOSAL{
 class I3PropagatorServicePROPOSAL : public I3PropagatorService {
 public:
 
-  virtual std::vector<I3Particle> Propagate(I3Particle& p, DiagnosticMapPtr frame);
-
-  virtual void SetRandomNumberGenerator(I3RandomServicePtr random);
-
   SET_LOGGER("I3PropagatorService");
 
-  /**
-   * @param mediadef[in] Path the the media definition file. If unspecified, this will
-   *                     default to $I3_BUILD/PROPOSAL/resources/configuration
-   * @param tabledir[in] Path to a directory in which to store interpolation
-   *                     constants for cross-section integrals. If unspecified, this will
-   *                     default to $I3_BUILD/PROPOSAL/resources/tables/
-   * @param cylinderRadius[in] Radius of the target volume in meters
-   * @param cylinderHeight[in] Full height of the target volume in meters
-   * @param type[in] Type of particle to propagate.
-   * @param particleMass[in] Mass of the propagated particle in GeV. This is
-   *                         only used if type is something exotic.
-   * @param brems_param[in] Parametrization of the bremsstrahlung cross-section to use
-   * @param photo_param[in] Parametrization of the photonuclear cross-section to use.
-   *
-   * The choice of parametrizations is discussed in <a href="http://arxiv.org/abs/hep-ph/0407075">MMC paper</a>.
-   */
-  I3PropagatorServicePROPOSAL(std::string mediadef=""
-      , std::string tabledir=""
-      , double cylinderRadius=800*I3Units::m
-      , double cylinderHeight=1600*I3Units::m
-      , I3Particle::ParticleType type=I3Particle::MuMinus
-      , double particleMass=NAN
-      , double particleCharge_=NAN
-      , double particleLifetime_=NAN
-      , ParametrizationType::Enum brems_param = ParametrizationType::BremsKelnerKokoulinPetrukhin
-      , ParametrizationType::Enum photo_param = ParametrizationType::PhotoAbramowiczLevinLevyMaor97ShadowButkevich
-  );
+public:
 
+  I3PropagatorServicePROPOSAL(I3Particle::ParticleType, std::string configfile="");
 
-  static std::string GetDefaultMediaDef();
-  static std::string GetDefaultTableDir();
   virtual ~I3PropagatorServicePROPOSAL();
 
+  virtual std::vector<I3Particle> Propagate(I3Particle& p, DiagnosticMapPtr frame, I3FramePtr);
   void SetTearDownPerCall(bool f) { tearDownPerCall_ = f; }
+  virtual void SetRandomNumberGenerator(I3RandomServicePtr random);
+
+  static std::string GetDefaultConfigFile();
 
  private:
-
-  Propagator *proposal;
-  double particleMass_;
-  double particleCharge_;
-  double particleLifetime_;
-  PROPOSALParticle* particle_type;
-
-  std::string mediadef_;
-  std::string tabledir_;
-
-  double cylinderRadius_;
-  double cylinderHeight_;
-
-  ParametrizationType::Enum brems_param_;
-  ParametrizationType::Enum photo_param_;
 
   /** @brief Tear down and re-initialize the propagator on every call to Propagate().
    * This is used for tests that ensure that propagation results do not depend
    * on event-to-event state.
    */
+
   bool tearDownPerCall_;
   I3RandomServicePtr rng_;
+
+  ParticleDef particle_def_;
+  std::string config_file_;
+  Propagator *proposal_;
+
 
   // default, assignment, and copy constructor declared private
   // I3PropagatorServicePROPOSAL();
   I3PropagatorServicePROPOSAL(const I3PropagatorServicePROPOSAL&);
   I3PropagatorServicePROPOSAL& operator=(const I3PropagatorServicePROPOSAL&);
 
-  ParticleType::Enum GeneratePROPOSALType(const I3Particle& p);
-  I3Particle::ParticleType GenerateI3Type(ParticleType::Enum ptype_PROPOSAL);
+  ParticleDef GeneratePROPOSALParticleDef(I3Particle::ParticleType);
+  I3Particle::ParticleType GenerateI3Type(const PROPOSAL::DynamicData&);
 
   /**
    *Fills an I3MMCTrackList with position, time, and energy
    *information if the particle is a muon or a tau.
    */
-  boost::shared_ptr<I3MMCTrack> GenerateMMCTrack(PROPOSALParticle* particle);
+  boost::shared_ptr<I3MMCTrack> GenerateMMCTrack(PROPOSAL::Particle* particle);
 
   boost::shared_ptr<I3MMCTrack> propagate(I3Particle& p, std::vector<I3Particle>& daughters);
 
