a92e699304f2df50807cbac685c6bd4fa0dc1893
mario <mario.dunsch@tu-dortmund.de>
2017-11-21 15:24:25 +0100

    Added serialize and deserialize functions to rng

--- private/PROPOSAL/math/RandomGenerator.cxx
+++ private/PROPOSAL/math/RandomGenerator.cxx
@@ -16,6 +16,11 @@ boost::random::mt19937 RandomGenerator::rng_;
 boost::variate_generator<boost::mt19937&, boost::uniform_real<> > RandomGenerator::variate_real(RandomGenerator::rng_, boost::uniform_real<>(0.0, 1.0));
 
 
+// ------------------------------------------------------------------------- //
+// Constructor & destructor
+// ------------------------------------------------------------------------- //
+
+
 RandomGenerator::RandomGenerator()
     : random_function(&RandomGenerator::DefaultRandomDouble)
 {
@@ -25,23 +30,45 @@ RandomGenerator::~RandomGenerator()
 {
 }
 
+// ------------------------------------------------------------------------- //
+// Methods
+// ------------------------------------------------------------------------- //
+
+
+// ------------------------------------------------------------------------- //
 double RandomGenerator::RandomDouble()
 {
     return random_function();
 }
 
+// ------------------------------------------------------------------------- //
 void RandomGenerator::SetSeed(int seed)
 {
     boost::mt19937::result_type s = static_cast<boost::mt19937::result_type>(seed);
     rng_.seed(s);
 }
 
+// ------------------------------------------------------------------------- //
+void RandomGenerator::Serialize(std::ostream& os)
+{
+    os << rng_;
+}
+
+// ------------------------------------------------------------------------- //
+void RandomGenerator::Deserialize(std::istream& is)
+{
+    is >> rng_;
+}
+
+// ------------------------------------------------------------------------- //
 void RandomGenerator::SetRandomNumberGenerator(boost::function<double ()> &f)
 {
     random_function = f;
 }
 
+// ------------------------------------------------------------------------- //
 double RandomGenerator::DefaultRandomDouble()
 {
     return variate_real();
 }
+
