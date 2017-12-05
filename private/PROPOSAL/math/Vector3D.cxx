ab5db7588a240f1d311c414767480200255cb27d
mario <mario.dunsch@tu-dortmund.de>
2017-12-05 18:05:33 +0100

    Added vector math operation to pybindigs

--- private/PROPOSAL/math/Vector3D.cxx
+++ private/PROPOSAL/math/Vector3D.cxx
@@ -170,6 +170,11 @@ Vector3D operator*(const Vector3D &vec1, const double factor1)
     return product;
 }
 
+double operator*(const Vector3D &vec1, const Vector3D &vec2)
+{
+    return scalar_product(vec1, vec2);
+}
+
 double scalar_product(const Vector3D &vec1, const Vector3D &vec2)
 {
     return vec1.x_*vec2.x_ + vec1.y_*vec2.y_ + vec1.z_*vec2.z_;
