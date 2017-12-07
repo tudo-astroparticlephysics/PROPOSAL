57014585bae77a04011eb255a712e861965dcbf1
mario <mario.dunsch@tu-dortmund.de>
2017-12-07 16:45:47 +0100

    Added license in every header.

--- public/PROPOSAL/Photonuclear.h
+++ public/PROPOSAL/Photonuclear.h
@@ -1,3 +1,31 @@
+
+/******************************************************************************
+ *																			  *
+ * This file is part of the simulation tool PROPOSAL.						  *
+ *																			  *
+ * Copyright (C) 2017 TU Dortmund University, Department of Physics,		  *
+ * 				      Chair Experimental Physics 5b							  *
+ *																			  *
+ * This software may be modified and distributed under the terms of a		  *
+ * modified GNU Lesser General Public Licence version 3 (LGPL),               *
+ * copied verbatim in the file "LICENSE". 									  *
+ *																			  *
+ * Modifcations to the LGPL License:										  *
+ *																			  *
+ *      1. The user shall acknowledge the use of PROPOSAL by citing the		  *
+ *         following reference:												  *
+ *																			  *
+ *         J.H. Koehne et al.  Comput.Phys.Commun. 184 (2013) 2070-2090 DOI:  *
+ *         10.1016/j.cpc.2013.04.001										  *
+ *																			  *
+ *      2. The user should report any bugs/errors or improvments to the       *
+ *         current maintainer of PROPOSAL or open an issue on the             *
+ *		   GitHub webpage													  *
+ *																			  *
+ *		   "https://github.com/tudo-astroparticlephysics/PROPOSAL"			  *
+ *																			  *
+ ******************************************************************************/
+
 #pragma once
 
 #ifndef Photonuclear_H
@@ -101,9 +129,9 @@ protected:
 //----------------------------------------------------------------------------//
 
     /*!
-     * parametrized cross section of the interaction 
+     * parametrized cross section of the interaction
      * between a nucleus and a real photon
-     * 
+     *
      * a parametrization is used only for high energy transfer
      * Caldwell in Phys.Rev.Let. 42 1979
      *
@@ -112,7 +140,7 @@ protected:
      */
 
     double PhotoNucleusCrossSectionCaldwell(double nu);
-    
+
     double PhotoNucleusCrossSectionKokoulin(double nu);
     double PhotoNucleusCrossSectionRhode(double nu);
     double PhotoNucleusCrossSectionBezrukovBugaev(double nu);
--- public/PROPOSAL/RootFinder.h
+++ public/PROPOSAL/RootFinder.h
@@ -1,13 +1,31 @@
-/**
- * Original comment of the Java class by Dmitry Chirkin:
- * This class provides routines for calculating roots by
- * the combination of the Newton-Raphson method and bisection.
- * Methods contained here are based on the Numerical Recipes
- * (W. H. Press et al.).
- *
- * @author Dmitry Chirkin
- * Ported to C++ Jan-Hendrik Köhne
- */
+
+/******************************************************************************
+ *																			  *
+ * This file is part of the simulation tool PROPOSAL.						  *
+ *																			  *
+ * Copyright (C) 2017 TU Dortmund University, Department of Physics,		  *
+ * 				      Chair Experimental Physics 5b							  *
+ *																			  *
+ * This software may be modified and distributed under the terms of a		  *
+ * modified GNU Lesser General Public Licence version 3 (LGPL),               *
+ * copied verbatim in the file "LICENSE". 									  *
+ *																			  *
+ * Modifcations to the LGPL License:										  *
+ *																			  *
+ *      1. The user shall acknowledge the use of PROPOSAL by citing the		  *
+ *         following reference:												  *
+ *																			  *
+ *         J.H. Koehne et al.  Comput.Phys.Commun. 184 (2013) 2070-2090 DOI:  *
+ *         10.1016/j.cpc.2013.04.001										  *
+ *																			  *
+ *      2. The user should report any bugs/errors or improvments to the       *
+ *         current maintainer of PROPOSAL or open an issue on the             *
+ *		   GitHub webpage													  *
+ *																			  *
+ *		   "https://github.com/tudo-astroparticlephysics/PROPOSAL"			  *
+ *																			  *
+ ******************************************************************************/
+
 #pragma once
 
 #ifndef ROOTFINDER_H
