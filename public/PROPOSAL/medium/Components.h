eaaeaf497c63ff35b08f5c7750ce56233dfd117b
mario <mario.dunsch@tu-dortmund.de>
2018-04-19 15:23:48 +0200

    Added licence to headers

--- public/PROPOSAL/medium/Components.h
+++ public/PROPOSAL/medium/Components.h
@@ -1,11 +1,31 @@
-/*! \file   Components.h
- *   \brief  Header file for definition of the components class object.
- *
- *   For more details see the class documentation.
- *
- *   \date   Sat Aug  5 14:47:16 CEST 2017
- *   \author Mario Dunsch
- */
+
+/******************************************************************************
+ *                                                                            *
+ * This file is part of the simulation tool PROPOSAL.                         *
+ *                                                                            *
+ * Copyright (C) 2017 TU Dortmund University, Department of Physics,          *
+ *                    Chair Experimental Physics 5b                           *
+ *                                                                            *
+ * This software may be modified and distributed under the terms of a         *
+ * modified GNU Lesser General Public Licence version 3 (LGPL),               *
+ * copied verbatim in the file "LICENSE".                                     *
+ *                                                                            *
+ * Modifcations to the LGPL License:                                          *
+ *                                                                            *
+ *      1. The user shall acknowledge the use of PROPOSAL by citing the       *
+ *         following reference:                                               *
+ *                                                                            *
+ *         J.H. Koehne et al.  Comput.Phys.Commun. 184 (2013) 2070-2090 DOI:  *
+ *         10.1016/j.cpc.2013.04.001                                          *
+ *                                                                            *
+ *      2. The user should report any bugs/errors or improvments to the       *
+ *         current maintainer of PROPOSAL or open an issue on the             *
+ *         GitHub webpage                                                     *
+ *                                                                            *
+ *         "https://github.com/tudo-astroparticlephysics/PROPOSAL"            *
+ *                                                                            *
+ ******************************************************************************/
+
 
 #pragma once
 
