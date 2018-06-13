590659c89193cb52c5193be9fdab8618a9d7c21a
mario <mario.dunsch@tu-dortmund.de>
2018-06-13 18:04:06 +0200

    fix: Get rid of low energy articfacts.
    In the low energy region of dE/dx the interpolation creates
    poles due to using rational interpolation. The reason is the use
    of too few nodes at low energy region.
    This is a hot fix which just increases the number of used nodes from
    100 to 200. The performance loss is about 5% at low energies (< 10^6) and
    below 1% at hight energies (> 10^6 MeV)

--- private/PROPOSAL/crossection/PhotoInterpolant.cxx
+++ private/PROPOSAL/crossection/PhotoInterpolant.cxx
@@ -30,7 +30,7 @@ PhotoInterpolant::PhotoInterpolant(const Photonuclear& param, InterpolationDef d
     // Needed for CalculatedEdx integration
     PhotoIntegral photo(param);
 
-    builder1d.SetMax(NUM1)
+    builder1d.SetMax(NUM2)
         .SetXMin(param.GetParticleDef().low)
         .SetXMax(BIGENERGY)
         .SetRomberg(def.order_of_interpolation)
