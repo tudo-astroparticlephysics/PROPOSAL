00f45bf9333c8c7a8182bee3825c789744fd45ad
mario <mario.dunsch@tu-dortmund.de>
2018-06-26 15:06:42 +0200

    alias: add weights to many body decay
    There must be still a bug.
    The energies are to high for the secondaries.

--- private/PROPOSAL/decay/ManyBodyPhaseSpace.cxx
+++ private/PROPOSAL/decay/ManyBodyPhaseSpace.cxx
@@ -9,6 +9,8 @@
 #include "PROPOSAL/math/RandomGenerator.h"
 #include "PROPOSAL/particle/Particle.h"
 
+#include <boost/math/special_functions/factorials.hpp>
+
 using namespace PROPOSAL;
 
 const std::string ManyBodyPhaseSpace::name_ = "ManyBodyPhaseSpace";
@@ -17,6 +19,7 @@ ManyBodyPhaseSpace::ManyBodyPhaseSpace(std::vector<const ParticleDef*> daughters
     : DecayChannel()
     , daughters_()
     , daughter_masses_()
+    , number_of_daughters_(static_cast<int>(daughters.size()))
     , sum_daughter_masses_(0.0)
 {
     for (std::vector<const ParticleDef*>::iterator it = daughters.begin(); it != daughters.end(); ++it)
@@ -41,6 +44,7 @@ ManyBodyPhaseSpace::ManyBodyPhaseSpace(const ManyBodyPhaseSpace& mode)
     : DecayChannel(mode)
     , daughters_()
     , daughter_masses_(mode.daughter_masses_)
+    , number_of_daughters_(mode.number_of_daughters_)
     , sum_daughter_masses_(mode.sum_daughter_masses_)
 {
     for (std::vector<const ParticleDef*>::const_iterator it = mode.daughters_.begin(); it != mode.daughters_.end();
@@ -60,6 +64,9 @@ bool ManyBodyPhaseSpace::compare(const DecayChannel& channel) const
     } else if (daughter_masses_.size() != many_body->daughter_masses_.size())
     {
         return false;
+    } else if (number_of_daughters_ != many_body->number_of_daughters_)
+    {
+        return false;
     } else if (daughter_masses_ != many_body->daughter_masses_)
     {
         return false;
@@ -72,11 +79,8 @@ bool ManyBodyPhaseSpace::compare(const DecayChannel& channel) const
 DecayChannel::DecayProducts ManyBodyPhaseSpace::Decay(const Particle& particle)
 {
     double parent_mass = particle.GetMass();
-    std::vector<double> virtual_masses;
 
-    std::vector<double> randoms;
-    randoms.reserve(daughters_.size());
-
+    // Create vector for decay products
     DecayProducts products;
     products.reserve(daughters_.size());
 
@@ -85,43 +89,37 @@ DecayChannel::DecayProducts ManyBodyPhaseSpace::Decay(const Particle& particle)
         products.push_back(new Particle(**it));
     }
 
-    // Create sorted random numbers
-    randoms.push_back(0.0);
+    // Create vector for virtual masses
+    std::vector<double> virtual_masses;
 
-    for (unsigned int i = 0; i < daughter_masses_.size() - 2; ++i)
+    // Create vector for intermediate momenta
+    std::vector<double> momenta;
+
+    // prefactor for the phase space density
+    PhaseSpaceParameters params = GetPhaseSpaceParams(particle.GetParticleDef());
+
+    // do rejection until phase space distribution is uniform
+    double weight = -1.0;
+
+    while (RandomGenerator::Get().RandomDouble() * params.weight_max > weight)
     {
-        randoms.push_back(RandomGenerator::Get().RandomDouble());
+        virtual_masses = CalculateVirtualMasses(parent_mass);
+        momenta = CalculateIntermediateMomenta(weight, params.normalization, virtual_masses);
     }
 
-    randoms.push_back(1.0);
-
-    std::sort(randoms.begin(), randoms.end());
-
-    // Calculate virtual masses
-    double intermediate_mass = 0.0;
-    for (unsigned int i = 0; i < daughter_masses_.size(); ++i)
-    {
-        intermediate_mass += daughter_masses_[i];
-        virtual_masses.push_back(intermediate_mass + randoms[i] * (parent_mass - sum_daughter_masses_));
-    }
-
-    // Calculate first momentum R2
-    unsigned int i = 1;
-
-    double momentum    = Momentum(virtual_masses[i], virtual_masses[i - 1], daughter_masses_[i]);
     Vector3D direction = GenerateRandomDirection();
 
-    products[i]->SetDirection(direction);
-    products[i]->SetMomentum(momentum);
+    // Calculate first momentum in R2
+    products[1]->SetDirection(direction);
+    products[1]->SetMomentum(momenta[0]);
 
-    products[i - 1]->SetDirection(-direction);
-    products[i - 1]->SetMomentum(momentum);
+    products[0]->SetDirection(-direction);
+    products[0]->SetMomentum(momenta[0]);
 
+    // Correct the previous momenta
     for (unsigned int i = 2; i < daughter_masses_.size(); ++i)
     {
-        // Generate momentum
-
-        double momentum = Momentum(virtual_masses[i], virtual_masses[i - 1], daughter_masses_[i]);
+        double momentum = momenta[i-1];
 
         products[i]->SetDirection(GenerateRandomDirection());
         products[i]->SetMomentum(momentum);
@@ -144,6 +142,102 @@ DecayChannel::DecayProducts ManyBodyPhaseSpace::Decay(const Particle& particle)
     CopyParticleProperties(products, particle);
 
     return products;
+}
+
+// ------------------------------------------------------------------------- //
+ManyBodyPhaseSpace::PhaseSpaceParameters ManyBodyPhaseSpace::GetPhaseSpaceParams(const ParticleDef& parent)
+{
+    ParameterMap::iterator it = parameter_map_.find(parent);
+
+    if (it != parameter_map_.end())
+    {
+        return it->second;
+    } else
+    {
+        PhaseSpaceParameters params;
+        params.normalization = CalculateNormalization(parent.mass);
+        params.weight_max = EstimateMaxWeight(params.normalization, parent.mass);
+        parameter_map_[parent] = params;
+
+        return params;
+    }
+}
+
+// ------------------------------------------------------------------------- //
+double ManyBodyPhaseSpace::CalculateNormalization(double parent_mass)
+{
+    double normalization = std::pow(parent_mass - sum_daughter_masses_, number_of_daughters_ - 2) /
+                           boost::math::factorial<double>(number_of_daughters_ - 2) *
+                           std::pow(2.0 * boost::math::constants::pi<double>(), number_of_daughters_ - 1.0);
+
+    return normalization /= 2.0 * parent_mass;
+}
+
+// ------------------------------------------------------------------------- //
+double ManyBodyPhaseSpace::EstimateMaxWeight(double normalization, double parent_mass)
+{
+    double weight = 1.0;
+    double E_max = parent_mass - sum_daughter_masses_ + daughter_masses_[0];
+    double E_min = 0.0;
+
+    for (int i = 1; i < number_of_daughters_; ++i)
+    {
+        E_min += daughter_masses_[i - 1];
+        E_max += daughter_masses_[i];
+        weight *= Momentum(E_max, E_min, daughter_masses_[i]);
+    }
+
+    return normalization * weight;
+}
+
+// ------------------------------------------------------------------------- //
+std::vector<double> ManyBodyPhaseSpace::CalculateVirtualMasses(double parent_mass)
+{
+    std::vector<double> randoms;
+    randoms.reserve(daughters_.size());
+
+    std::vector<double> virtual_masses;
+
+    // Create sorted random numbers
+    randoms.push_back(0.0);
+
+    for (unsigned int i = 0; i < daughter_masses_.size() - 2; ++i)
+    {
+        randoms.push_back(RandomGenerator::Get().RandomDouble());
+    }
+
+    randoms.push_back(1.0);
+
+    std::sort(randoms.begin(), randoms.end());
+
+    // Calculate virtual masses
+    double intermediate_mass = 0.0;
+    for (int i = 0; i < number_of_daughters_; ++i)
+    {
+        intermediate_mass += daughter_masses_[i];
+        virtual_masses.push_back(intermediate_mass + randoms[i] * (parent_mass - sum_daughter_masses_));
+    }
+
+    return virtual_masses;
+}
+
+// ------------------------------------------------------------------------- //
+std::vector<double> ManyBodyPhaseSpace::CalculateIntermediateMomenta(double& weight, double normalization, std::vector<double>& virtual_masses)
+{
+    weight = 1.0;
+    double momentum = 0.0;
+    std::vector<double> momenta;
+
+    for (int i = 1; i < number_of_daughters_; ++i)
+    {
+        momentum = Momentum(virtual_masses[i], virtual_masses[i - 1], daughter_masses_[i]);
+        momenta.push_back(momentum);
+        weight *= momentum;
+    }
+
+    weight = normalization * weight;
+
+    return momenta;
 }
 
 // ------------------------------------------------------------------------- //
