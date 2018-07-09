1dd5aebfd620933e28b287cce2318efbc107c40c
sudojan <jan.soedingrekso@tu-dortmund.de>
2018-07-09 16:50:36 +0200

    correct all icesim python scrips to newest version

--- cmake/I3_CMakeLists.cmake
+++ cmake/I3_CMakeLists.cmake
@@ -9,27 +9,29 @@
 
 # file(GLOB_RECURSE PROPOSAL_SRC_FILES ${PROJECT_SOURCE_DIR}/PROPOSAL/private/PROPOSAL/*)
 # message(STATUS ${PROPOSAL_SRC_FILES})
-i3_project(PROPOSAL)
+i3_project(PROPOSAL
+    DOCS_DIR doc
+)
 
 file(GLOB_RECURSE PROPOSAL_SRC_FILES ${PROJECT_SOURCE_DIR}/private/PROPOSAL/*)
 file(GLOB_RECURSE I3_PROPOSAL_SRC_FILES ${PROJECT_SOURCE_DIR}/private/PROPOSAL-icetray/*)
 
 i3_add_library(PROPOSAL
-		${PROPOSAL_SRC_FILES}
-        private/PROPOSAL-icetray/I3PropagatorServicePROPOSAL.cxx
-        private/PROPOSAL-icetray/SimplePropagator.cxx
-		private/PROPOSAL-icetray/Converter.cxx
-		# ${I3_PROPOSAL_SRC_FILES}
+    ${PROPOSAL_SRC_FILES}
+    private/PROPOSAL-icetray/I3PropagatorServicePROPOSAL.cxx
+    private/PROPOSAL-icetray/SimplePropagator.cxx
+    private/PROPOSAL-icetray/Converter.cxx
+    # ${I3_PROPOSAL_SRC_FILES}
 
-        USE_TOOLS boost
-        USE_PROJECTS icetray serialization dataclasses sim-services simclasses phys-services
-        )
+    USE_TOOLS boost
+    USE_PROJECTS icetray serialization dataclasses sim-services simclasses phys-services
+)
 
-set_target_properties(PROPOSAL PROPERTIES COMPILE_FLAGS "${CMAKE_CXX_FLAGS}  -Wall")
+set_target_properties(PROPOSAL PROPERTIES COMPILE_FLAGS "${CMAKE_CXX_FLAGS}  -Wall -Werror")
 
 i3_add_pybindings(PROPOSAL
-        private/PROPOSAL-icetray/pybindings.cxx
-        USE_PROJECTS PROPOSAL
+    private/PROPOSAL-icetray/pybindings.cxx
+    USE_PROJECTS PROPOSAL
 )
 
 # Pre-generate parameterization tables for use in read-only environments
@@ -37,20 +39,19 @@ add_custom_command(OUTPUT resources/tables/.tables.auto_generated COMMAND ${CMAK
 add_custom_target(PROPOSAL-tables ALL DEPENDS resources/tables/.tables.auto_generated)
 
 set(LIB_${PROJECT_NAME}_TESTS
-        private/PROPOSAL-icetray/test/main.cxx
+    private/PROPOSAL-icetray/test/main.cxx
 )
 
 if (SPRNG_FOUND)
-        # this test requires SPRNG
-        LIST(APPEND LIB_${PROJECT_NAME}_TESTS
-                private/PROPOSAL-icetray/test/Repeatability.cxx
-        )
+    # this test requires SPRNG
+    LIST(APPEND LIB_${PROJECT_NAME}_TESTS
+        private/PROPOSAL-icetray/test/Repeatability.cxx
+    )
 endif (SPRNG_FOUND)
 
 
 i3_test_executable(test
-        ${LIB_${PROJECT_NAME}_TESTS}
-        USE_TOOLS boost gsl python
-        USE_PROJECTS PROPOSAL icetray dataclasses phys-services
+    ${LIB_${PROJECT_NAME}_TESTS}
+    USE_TOOLS boost gsl python
+    USE_PROJECTS PROPOSAL icetray dataclasses phys-services
 )
-
