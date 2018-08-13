c243c83d536b1bbf798fc1870ed37bb61efb3afc
mario <mario.dunsch@tu-dortmund.de>
2018-08-13 17:29:02 +0200

    alias: add new example scripts

---
+++ resources/examples/standalone/plot_tau_decay_hadrons.py
@@ -0,0 +1,224 @@
+import pyPROPOSAL as pp
+import time
+
+try:
+    import matplotlib as mpl
+    import matplotlib.pyplot as plt
+    from matplotlib import colors
+except ImportError:
+    raise ImportError("Matplotlib not installed!")
+
+import numpy as np
+
+data_formfactor = np.array([
+    [0.6, 0.7, 0.75, 1.0, 1.6, 2.45],
+    [0.493, 0.471, 0.407, 0.351, 0.243, 0.167]
+])
+
+
+# form_factor = np.poly1d(np.polyfit(1e6 * data_formfactor[0], data_formfactor[1], 3))
+
+def form_factor(x):
+    term = 1. + 0.0306 * x + (0.0194 * x**3) / (1. + x)
+    return np.exp(-1.171 * x**(0.536)) * term
+
+leptons = [
+    pp.NuTauDef.get(),
+    pp.NuTauBarDef.get(),
+    pp.NuMuDef.get(),
+    pp.NuMuBarDef.get(),
+    pp.NuEDef.get(),
+    pp.NuEBarDef.get(),
+    pp.TauMinusDef.get(),
+    pp.TauPlusDef.get(),
+    pp.MuMinusDef.get(),
+    pp.MuPlusDef.get(),
+    pp.EMinusDef.get(),
+    pp.EPlusDef.get(),
+]
+
+
+def filter_hadr(secondarys):
+    prods = [p for p in secondarys if p.id == pp.Data.Particle]
+    E = [p.energy for p in prods if p.particle_def not in leptons]
+    return sum(E)
+
+
+def filter_particle(secondarys, particle):
+    prods = [p for p in secondarys if p.id == pp.Data.Particle]
+    E = [p.energy for p in prods if p.particle_def == particle]
+    return sum(E)
+
+
+def filter_lep(secondarys):
+    prods = [p for p in secondarys if p.id == pp.Data.Particle]
+    E = [p.energy for p in prods if p.particle_def in leptons]
+    return sum(E)
+
+
+def scalar_product(a, b):
+    return a[0] * b[0] - a[1] * b[1]
+
+
+def add(a, b):
+    return (a[0] + b[0], a[1] + b[1])
+
+
+def evaluate(particle, products):
+    G_F = 1.1663787*1e-2  # MeV
+    V_ud = 0.97427
+
+    tau = particle
+    pi1 = products[0]
+    pi2 = products[1]
+    nu = products[2]
+
+    P = (tau.energy, tau.momentum * tau.direction)
+    p1 = (pi1.energy, pi1.momentum * pi1.direction)
+    p2 = (pi2.energy, pi2.momentum * pi2.direction)
+    k = (nu.energy, nu.momentum * nu.direction)
+
+    p12 = add(p1, (-1.0 * p2[0], -1.0 * p2[1]))
+    q = add(p1, p2)
+    q2 = scalar_product(q, q)
+    term1 = scalar_product(k, p12)
+    term2 = scalar_product(P, p12)
+    term3 = scalar_product(P, k) * scalar_product(p12, p12)
+
+    Y = 2. * term1 * term2 - term3
+
+    return 4. * (G_F * V_ud * form_factor(q2))**2 * Y
+
+if __name__ == "__main__":
+
+    # =========================================================
+    # 	Save energies
+    # =========================================================
+
+    statistics = int(1e1)
+    binning = 50
+
+    tau = pp.Particle(pp.TauMinusDef.get())
+    tau.direction = pp.Vector3D(0, 0, -1)
+
+    products = [
+        pp.Pi0Def.get(),
+        pp.PiMinusDef.get(),
+        pp.NuTauDef.get()
+    ]
+
+    products_particles = [pp.Particle(p) for p in products]
+    for p in products_particles:
+        p.direction = pp.Vector3D(0, 0, -1)
+        p.energy = 1e2
+
+    ME_func = pp.matrix_element_function(evaluate)
+    print(ME_func(tau, products_particles))
+
+    ME = pp.ManyBodyPhaseSpace(products)
+
+    ME.set_matrix_element(ME_func)
+    ME.set_uniform_sampling(True)
+
+    E_lep_pi0 = []
+    E_lep_pim = []
+    E_lep_nu = []
+
+    s1 = []
+    s2 = []
+
+    passed_time = 0.0
+
+    for i in range(statistics):
+        tau.position = pp.Vector3D(0, 0, 0)
+        tau.direction = pp.Vector3D(0, 0, -1)
+        tau.energy = tau.particle_def.mass
+        tau.propagated_distance = 0
+
+        t = time.time()
+
+        d = ME.decay(tau)
+        E_lep_pi0.append(filter_particle(d, pp.Pi0Def.get()))
+        E_lep_pim.append(filter_particle(d, pp.PiMinusDef.get()))
+        E_lep_nu.append(filter_particle(d, pp.NuTauDef.get()))
+
+        p1 = add((d[0].energy, d[0].momentum * d[0].direction), (d[1].energy, d[1].momentum * d[1].direction))
+        p2 = add((d[1].energy, d[1].momentum * d[1].direction), (d[2].energy, d[2].momentum * d[2].direction))
+
+        s1.append(
+            scalar_product(p1, p1)
+        )
+
+        s2.append(
+            scalar_product(p2, p2)
+        )
+
+        passed_time += time.time() - t
+
+    print("needed time = {}s".format(passed_time))
+
+    # =========================================================
+    # 	Plot
+    # =========================================================
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+
+    hist = ax.hist(
+        np.array(E_lep_pi0) / tau.energy,
+        histtype="step",
+        bins=binning,
+        color='k',
+        ls='-',
+        label=r"$\pi^0$"
+    )
+
+    hist = ax.hist(
+        np.array(E_lep_pim) / tau.energy,
+        histtype="step",
+        bins=binning,
+        color='k',
+        ls='-.',
+        label=r"$\pi^-$"
+    )
+
+    hist = ax.hist(
+        np.array(E_lep_nu) / tau.energy,
+        histtype="step",
+        bins=binning,
+        color='k',
+        ls=':',
+        label=r"$\nu$"
+    )
+
+    ax.set_ylabel(r'count')
+    ax.legend(loc='upper left')
+
+    fig.tight_layout()
+
+    fig.savefig("hadronic.pdf")
+
+    # =========================================================
+    # 	Dalitz Plot
+    # =========================================================
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+
+    hist = ax.hist2d(
+        s1,
+        s2,
+        bins=500,
+        norm=colors.LogNorm()
+    )
+
+    plt.colorbar(hist[3], ax=ax, pad=0.01)
+
+    ax.set_xlabel(r'$s_1 = (p_1 + p_2)^2$ / $\rm{GeV}^2$')
+    ax.set_ylabel(r'$s_2 = (p_2 + p_3)^2$ / $\rm{GeV}^2$')
+
+    fig.tight_layout()
+
+    fig.savefig("dalitz.pdf")
+
+    plt.show()
---
+++ resources/examples/standalone/plot_tau_decay_leptons.py
@@ -0,0 +1,174 @@
+import pyPROPOSAL as pp
+import time
+
+try:
+    import matplotlib as mpl
+    import matplotlib.pyplot as plt
+except ImportError:
+    raise ImportError("Matplotlib not installed!")
+
+import numpy as np
+
+leptons = [
+    pp.NuTauDef.get(),
+    pp.NuTauBarDef.get(),
+    pp.NuMuDef.get(),
+    pp.NuMuBarDef.get(),
+    pp.NuEDef.get(),
+    pp.NuEBarDef.get(),
+    pp.TauMinusDef.get(),
+    pp.TauPlusDef.get(),
+    pp.MuMinusDef.get(),
+    pp.MuPlusDef.get(),
+    pp.EMinusDef.get(),
+    pp.EPlusDef.get(),
+]
+
+
+def filter_hadr(secondarys):
+    prods = [p for p in secondarys if p.id == pp.Data.Particle]
+    E = [p.energy for p in prods if p.particle_def not in leptons]
+    return sum(E)
+
+
+def filter_particle(secondarys, particle):
+    prods = [p for p in secondarys if p.id == pp.Data.Particle]
+    E = [p.energy for p in prods if p.particle_def == particle]
+    return sum(E)
+
+
+def filter_lep(secondarys):
+    prods = [p for p in secondarys if p.id == pp.Data.Particle]
+    E = [p.energy for p in prods if p.particle_def in leptons]
+    return sum(E)
+
+
+def evaluate(particle, products):
+    G_F = 1.1663787*1e-2  # MeV
+
+    mu = particle
+    e = products[0]
+    numu = products[1]
+    nue = products[2]
+
+    p1 = mu.energy * nue.energy - (mu.momentum * mu.direction) * (nue.momentum * nue.direction)
+    p2 = e.energy * numu.energy - (e.momentum * e.direction) * (numu.momentum * numu.direction)
+
+    return 64 * G_F**2 * p1 * p2
+
+
+if __name__ == "__main__":
+
+    # =========================================================
+    # 	Save energies
+    # =========================================================
+
+    statistics = int(1e6)
+    binning = 50
+
+    mu = pp.Particle(pp.MuMinusDef.get())
+    mu.direction = pp.Vector3D(0, 0, -1)
+
+    products = [pp.EMinusDef.get(), pp.NuMuDef.get(), pp.NuEBarDef.get()]
+
+    ME = pp.matrix_element_function(evaluate)
+
+    lep_ME = pp.ManyBodyPhaseSpace(products)
+
+    lep_ME.set_matrix_element(ME)
+    lep_ME.set_uniform_sampling(True)
+
+    lep = pp.LeptonicDecayChannelApprox(*products)
+
+    E_lep_ME = []
+    E_lep = []
+
+    passed_time = 0.0
+
+    for i in range(statistics):
+        mu.position = pp.Vector3D(0, 0, 0)
+        mu.direction = pp.Vector3D(0, 0, -1)
+        mu.energy = mu.particle_def.mass
+        mu.propagated_distance = 0
+
+        t = time.time()
+
+        d = lep_ME.decay(mu)
+        E_lep_ME.append(filter_particle(d, pp.EMinusDef.get()))
+
+        mu.position = pp.Vector3D(0, 0, 0)
+        mu.direction = pp.Vector3D(0, 0, -1)
+        mu.energy = mu.particle_def.mass
+        mu.propagated_distance = 0
+
+        d = lep.decay(mu)
+        E_lep.append(filter_particle(d, pp.EMinusDef.get()))
+
+        passed_time += time.time() - t
+
+    print("needed time = {}s".format(passed_time))
+
+    # =========================================================
+    # 	Plot
+    # =========================================================
+
+    fig = plt.figure()
+    gs = mpl.gridspec.GridSpec(2, 1, height_ratios=[2, 1])
+
+    ax = fig.add_subplot(gs[0])
+
+    hist_ME = ax.hist(
+        np.array(E_lep) / mu.energy,
+        histtype="step",
+        # log=True,
+        bins=binning,
+        color='k',
+        ls='-',
+        label="decay width"
+    )
+
+    hist = ax.hist(
+        np.array(E_lep_ME) / mu.energy,
+        histtype="step",
+        # log=True,
+        bins=binning,
+        color='k',
+        ls='-.',
+        label="phase space"
+    )
+
+    ax.set_ylabel(r'count')
+
+    ax.legend(loc='upper left')
+
+    # ====[ ratio ]=============================================
+
+    ax = fig.add_subplot(gs[1], sharex=ax)
+
+    diff = hist_ME[0] / hist[0]
+    diff[np.isnan(diff)] = 1.0
+
+    bins = hist[1]
+    bincenters = 0.5*(bins[1:]+bins[:-1])
+
+    ax.plot(
+        bincenters,
+        np.nan_to_num(diff),
+        drawstyle="steps-mid",
+        color='k',
+        lw=1.0,
+        label="phase space / decay width"
+    )
+
+    ax.set_xlabel(r'final energy / MeV')
+    ax.set_ylabel(r'ratio')
+
+    ax.legend(loc='lower right')
+
+    ax.axhline(1.0, color='k', lw=0.5, ls='-.')
+
+    fig.tight_layout()
+
+    fig.savefig("leptonic.pdf")
+
+    plt.show()
