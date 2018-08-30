2cc33ca9621be05c14d1639c2b8b70f5f8a9b94d
mario <mario.dunsch@tu-dortmund.de>
2018-08-30 16:38:42 +0200

    alias: travis. log4cplus version

--- install_log4cplus.sh
+++ install_log4cplus.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -ex
 wget https://github.com/log4cplus/log4cplus/archive/REL_2_0_2.zip
-unzip REL_1_2_1.zip && cd log4cplus-REL_1_2_1
+unzip REL_2_0_2.zip && cd log4cplus-REL_2_0_2
 mkdir build && cd build && cmake .. && make && sudo make install
