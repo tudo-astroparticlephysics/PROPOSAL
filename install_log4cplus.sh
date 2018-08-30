bd722dace343cc21f1dda387388df6f5f3020e7b
mario <mario.dunsch@tu-dortmund.de>
2018-08-30 17:03:14 +0200

    alias: travis. log4cplus: wget -> clone recursive

--- install_log4cplus.sh
+++ install_log4cplus.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -ex
-wget https://github.com/log4cplus/log4cplus/archive/REL_2_0_1.zip
-unzip REL_2_0_1.zip && cd log4cplus-REL_2_0_1
+mkdir log4cplus && cd log4cplus
+git clone --recursive https://github.com/log4cplus/log4cplus.git --branch REL_2_0_2 .
 mkdir build && cd build && cmake .. && make && sudo make install
