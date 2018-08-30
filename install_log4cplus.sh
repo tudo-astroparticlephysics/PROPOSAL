c7fd10d427764042e8dcd8f0f556f4bbbc0fbe70
mario <mario.dunsch@tu-dortmund.de>
2018-08-30 16:48:36 +0200

    alias: travis. bug in log4cplus? 2_0_2 -> 2_0_1

--- install_log4cplus.sh
+++ install_log4cplus.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -ex
-wget https://github.com/log4cplus/log4cplus/archive/REL_2_0_2.zip
-unzip REL_2_0_2.zip && cd log4cplus-REL_2_0_2
+wget https://github.com/log4cplus/log4cplus/archive/REL_2_0_1.zip
+unzip REL_2_0_1.zip && cd log4cplus-REL_2_0_1
 mkdir build && cd build && cmake .. && make && sudo make install
