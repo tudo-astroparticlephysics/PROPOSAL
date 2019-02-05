731a0c720c182daba75790c065103fcd52f5b0fd
Jean-Marco Alameddine <jean-marco.alameddine@udo.edu>
2019-02-05 11:31:34 +0100

    Implemented specific MuPair CrossSection

--- private/PROPOSAL/crossection/parametrization/MupairProduction.cxx
+++ private/PROPOSAL/crossection/parametrization/MupairProduction.cxx
@@ -75,6 +75,7 @@ Parametrization::IntegralLimits MupairProduction::GetIntegralLimits(double energ
 {
     IntegralLimits limits;
 
+    //TODO: Check Integral Limits (where do we need MMU, where do we need the particle_def_.mass ?)
     limits.vMin = 2 * MMU / energy;
     limits.vMax = 1 - particle_def_.mass / energy;
 
@@ -93,11 +94,6 @@ Parametrization::IntegralLimits MupairProduction::GetIntegralLimits(double energ
     return limits;
 }
 
-
-// ------------------------------------------------------------------------- //
-// Parametrization of Kelner/Kokoulin/Petrukhin
-// Proc. 12th ICCR (1971), 2436
-// ------------------------------------------------------------------------- //
 
 /******************************************************************************
  *                          MupairProduction Integral                           *
@@ -140,6 +136,7 @@ double MupairProductionRhoIntegral::DifferentialCrossSection(double energy, doub
 {
     double rMax, aux;
 
+    //TODO: Check Integral Limits (where do we need MMU, where do we need the particle_def_.mass ?)
     aux  = 1 - 2 * MMU / (v * energy);
 
     if (aux > 0)
@@ -194,99 +191,59 @@ double MupairKelnerKokoulinPetrukhin::FunctionToIntegral(double energy, double v
     // DOI: 10.1134/1.1312894
 
 
-    double g1, g2;
-    double aux, aux1, aux2, r2;
-    double diagram_e, diagram_mu, atomic_electron_contribution;
+    double aux, aux1, aux2, r2, rMax, Z3, xi, beta, A_pow, r_mu;
+    double phi, U, U_max, X, Y;
     double medium_charge       = components_[component_index_]->GetNucCharge();
-    double medium_log_constant = components_[component_index_]->GetLogConstant();
+    double atomic_weight       = components_[component_index_]->GetAtomInMolecule();
+    //double medium_log_constant = components_[component_index_]->GetLogConstant();
+    double medium_log_constant = 183; // According to the paper, B is set to 183
 
     r           = 1 - r; // only for integral optimization - do not forget to swap integration limits!
     r2          = r * r;
-    double Z3   = std::pow(medium_charge, -1. / 3);
-    aux         = (particle_def_.mass * v) / (2 * ME);
-    double xi   = aux * aux * (1 - r2) / (1 - v);
-    double beta = (v * v) / (2 * (1 - v));
+    rMax        = 2 * MMU / (v * energy); // TODO: Is this correct with the swap of the integration limits AND with the MMU?
+    Z3          = std::pow(medium_charge, -1. / 3);
+    aux         = (particle_def_.mass * v) / (2 * MMU);
+    xi          = aux * aux * (1 - r2) / (1 - v);
+    beta        = (v * v) / (2 * (1 - v));
+    A_pow       = std::pow(atomic_weight, -0.27);
+    r_mu        = RE * ME / MMU; //classical muon radius
 
-    // these are the Y_e and Y_mu expressions in the original paper
-    diagram_e  = (5 - r2 + 4 * beta * (1 + r2)) / (2 * (1 + 3 * beta) * std::log(3 + 1 / xi) - r2 - 2 * beta * (2 - r2));
-    diagram_mu = (4 + r2 + 3 * beta * (1 + r2)) / ((1 + r2) * (1.5 + 2 * beta) * std::log(3 + xi) + 1 - 1.5 * r2);
+    //Phi Calculation (18)
+    aux     = (2 + r2) * (1 + beta) + xi * (3 + r2);
+    aux     *= std::log(1 + 1. / xi);
 
-    // these arae the L_e and L_mu expressions
-    aux        = (1.5 * ME) / (particle_def_.mass * Z3);
-    aux1       = (1 + xi) * (1 + diagram_e);
-    aux2       = (2 * ME * SQRTE * medium_log_constant * Z3) / (energy * v * (1 - r2));
-    diagram_e  = std::log((medium_log_constant * Z3 * std::sqrt(aux1)) / (1 + aux2 * aux1)) - 0.5 * std::log(1 + aux * aux * aux1);
-    diagram_mu = std::log((medium_log_constant / aux * Z3) / (1 + aux2 * (1 + xi) * (1 + diagram_mu)));
+    aux1    = (1 + r2) * (1 + 1.5 * beta) - 1. / xi * (1 + 2 * beta) * (1 - r2);
+    aux1    *= std::log(1 + xi);
+    aux2    = -1 - 3 * r2 + beta * (1 - 2 * r2);
 
-    // these are the Phi_e and Phi_mu expressions
-    // if the logarithms above are below zero, the contribution of the diagram is set to zero
-    // if lpm supression is taken into account, Phi_e is changed
-    if (diagram_e > 0)
+    phi     = aux + aux1 + aux2; 
+
+    //X Calculation (22)
+    Y       = 12 * std::sqrt(MMU / energy); //(21)
+    aux     = 0.65 * A_pow * medium_log_constant * Z3 * MMU / ME;
+    aux1    = 2 * SQRTE * std::pow(MMU, 2) * medium_log_constant * Z3 * (1 + xi) * (1 + Y);
+    aux2    = ME * energy * v * (1 - r2);
+
+    U       = aux / (1 + aux1 / aux2);
+
+    aux2    = ME * energy * v * (1 - rMax * rMax);
+    U_max   = aux / (1 + aux1 / aux2);
+
+    X       = 1 + U - U_max;
+
+    //Combine results
+    aux     = ALPHA * r_mu * particle_def_.charge * medium_charge;
+    aux     *= 2 * aux * phi * (1 - v) / (1.5 * PI * v); //Factor 2: Similar to factor 2 from EPairProduction, probably from symmetry in Rho
+
+    if (X > 0)
     {
-        if (1 / xi < HALF_PRECISION)
-        {
-            // TODO: where does this short expression come from?
-            diagram_e = (1.5 - r2 / 2 + beta * (1 + r2)) / xi * diagram_e;
-        } else
-        {
-            diagram_e =
-                (((2 + r2) * (1 + beta) + xi * (3 + r2)) * std::log(1 + 1 / xi) + (1 - r2 - beta) / (1 + xi) - (3 + r2)) *
-                diagram_e;
-        }
-    } else
+        aux *= std::log(X);
+    }
+    else
     {
-        diagram_e = 0;
+        aux = 0;
     }
 
-    if (diagram_mu > 0)
-    {
-        diagram_mu = (((1 + r2) * (1 + 1.5 * beta) - (1 + 2 * beta) * (1 - r2) / xi) * std::log(1 + xi) +
-                      xi * (1 - r2 - beta) / (1 + xi) + (1 + 2 * beta) * (1 - r2)) *
-                     diagram_mu;
-    } else
-    {
-        diagram_mu = 0;
-    }
-
-    // Calculating the contribution of atomic electrons as scattering partner
-    // Phys. Atom. Nucl. 61 (1998), 448
-    if (medium_charge == 1)
-    {
-        g1 = 4.4e-5;
-        g2 = 4.8e-5;
-    } else
-    {
-        g1 = 1.95e-5;
-        g2 = 5.3e-5;
-    }
-
-    aux  = energy / particle_def_.mass;
-    aux1 = 0.073 * std::log(aux / (1 + g1 * aux / (Z3 * Z3))) - 0.26;
-    aux2 = 0.058 * std::log(aux / (1 + g2 * aux / Z3)) - 0.14;
-
-    if (aux1 > 0 && aux2 > 0)
-    {
-        atomic_electron_contribution = aux1 / aux2;
-    } else
-    {
-        atomic_electron_contribution = 0;
-    }
-
-    // combining the results
-    aux = ALPHA * RE * particle_def_.charge;
-    ;
-    aux *= aux / (1.5 * PI) * 2 * medium_charge * (medium_charge + atomic_electron_contribution);
-    aux1 = ME / particle_def_.mass * particle_def_.charge;
-    ;
-    aux *= (1 - v) / v * (diagram_e + aux1 * aux1 * diagram_mu);
-
-    /*
-    if (lpm_)
-    {
-        aux *= lpm(energy, v, r2, beta, xi);
-    }
-    */
-    
     if (aux < 0)
     {
         aux = 0;
