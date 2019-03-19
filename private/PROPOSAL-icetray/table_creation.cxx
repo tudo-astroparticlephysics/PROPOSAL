52ad88bc3138f0fffa3623772ab9008905bc8112
sudojan <jan.soedingrekso@tu-dortmund.de>
2019-03-19 20:41:22 +0100

    adapt i3 files [ci skip]

--- private/PROPOSAL-icetray/table_creation.cxx
+++ private/PROPOSAL-icetray/table_creation.cxx
@@ -11,6 +11,9 @@ int main()
     I3PropagatorServicePROPOSALPtr prop(new I3PropagatorServicePROPOSAL);
     std::ofstream output;
     const char* I3_BUILD = getenv("I3_BUILD");
+    if (!I3_BUILD)
+        log_fatal("$I3_BUILD is not set!");
     std::string s(I3_BUILD);
     output.open(s + "/PROPOSAL/resources/tables/.tables.auto_generated");
+    output.close();
 }
