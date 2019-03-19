d7c8955b3e667a0952e16fe552881ee841ab7735
Jean-Marco Alameddine <jean-marco.alameddine@udo.edu>
2019-03-19 17:40:10 +0100

    Sample random numbers before function call in Sector.cxx [ci skip]

--- private/PROPOSAL/sector/Sector.cxx
+++ private/PROPOSAL/sector/Sector.cxx
@@ -427,11 +427,13 @@ double Sector::Propagate(double distance)
                     for (unsigned int i = 0; i < cross_sections.size(); i++)
                     {
                         if(DynamicData::MuPair == cross_sections[i]->GetTypeId()){
+                            double rnd1 = RandomGenerator::Get().RandomDouble();
+                            double rnd2 = RandomGenerator::Get().RandomDouble();
                             decay_products = cross_sections[i]->CalculateProducedParticles(
                                     final_energy,
                                     energy_loss.first,
-                                    RandomGenerator::Get().RandomDouble(),
-                                    RandomGenerator::Get().RandomDouble());
+                                    rnd1,
+                                    rnd2);
                             Output::getInstance().FillSecondaryVector(decay_products);
                             break;
                         }
