cbef2345f07a12da5b7bd5d62d7cbc3ceb4cdd80
Jean-Marco Alameddine <jean-marco.alameddine@udo.edu>
2019-05-13 17:03:22 +0200

    fix: Remove redundant tables from WeakInteraction We do not need to save the bare cross section (sigma) as an interpolation table: Every quantity that is derived from sigma is already saved in a seperate table.

--- private/PROPOSAL/crossection/parametrization/WeakInteraction.cxx
+++ private/PROPOSAL/crossection/parametrization/WeakInteraction.cxx
@@ -65,70 +65,18 @@ WeakCooperSarkarMertsch::WeakCooperSarkarMertsch(const ParticleDef& particle_def
         , interpolant_(2, NULL)
 {
 
-    std::vector<Interpolant2DBuilder_array_as> builder2d(2); // [0] for proton, [1] for neutron
-    Helper::InterpolantBuilderContainer builder_container2d(2);
     if(particle_def==EMinusDef::Get()||particle_def==MuMinusDef::Get()||particle_def==TauMinusDef::Get()){
-        builder2d[0]
-                .Setx1(energies)
-                .Setx2(y_nubar_p)
-                .Sety(sigma_nubar_p)
-                .SetRomberg1(IROMB)
-                .SetRational1(false)
-                .SetRelative1(false)
-                .SetRomberg2(IROMB)
-                .SetRational2(false)
-                .SetRelative2(false);
-
-        builder_container2d[0].first  = &builder2d[0];
-        builder_container2d[0].second = &interpolant_[0];
-
-        builder2d[1]
-                .Setx1(energies)
-                .Setx2(y_nubar_n)
-                .Sety(sigma_nubar_n)
-                .SetRomberg1(IROMB)
-                .SetRational1(false)
-                .SetRelative1(false)
-                .SetRomberg2(IROMB)
-                .SetRational2(false)
-                .SetRelative2(false);
-
-        builder_container2d[1].first  = &builder2d[1];
-        builder_container2d[1].second = &interpolant_[1];
+        // Initialize interpolant for particles (remember crossing symmetry rules)
+        interpolant_[0] = new Interpolant(energies, y_nubar_p, sigma_nubar_p, IROMB, false, false, IROMB, false, false);
+        interpolant_[1] = new Interpolant(energies, y_nubar_n, sigma_nubar_n, IROMB, false, false, IROMB, false, false);
     }
     else if(particle_def==EPlusDef::Get()||particle_def==MuPlusDef::Get()||particle_def==TauPlusDef::Get()){
-        builder2d[0]
-                .Setx1(energies)
-                .Setx2(y_nu_p)
-                .Sety(sigma_nu_p)
-                .SetRomberg1(IROMB)
-                .SetRational1(false)
-                .SetRelative1(false)
-                .SetRomberg2(IROMB)
-                .SetRational2(false)
-                .SetRelative2(false);
-
-        builder_container2d[0].first  = &builder2d[0];
-        builder_container2d[0].second = &interpolant_[0];
-
-        builder2d[1]
-                .Setx1(energies)
-                .Setx2(y_nu_n)
-                .Sety(sigma_nu_n)
-                .SetRomberg1(IROMB)
-                .SetRational1(false)
-                .SetRelative1(false)
-                .SetRomberg2(IROMB)
-                .SetRational2(false)
-                .SetRelative2(false);
-
-        builder_container2d[1].first  = &builder2d[1];
-        builder_container2d[1].second = &interpolant_[1];
+        // Initialize interpolant for antiparticles (remember crossing symmetry rules)
+        interpolant_[0] = new Interpolant(energies, y_nu_p, sigma_nu_p, IROMB, false, false, IROMB, false, false);
+        interpolant_[1] = new Interpolant(energies, y_nu_n, sigma_nu_n, IROMB, false, false, IROMB, false, false);
+    }else{
+        log_fatal("Weak interaction: Particle to propagate is not a SM charged lepton");
     }
-    else{
-        log_fatal("Weak interaction table_read: Particle to propagate is not a SM charged lepton");
-    }
-    Helper::InitializeInterpolation("WeakInt", builder_container2d, std::vector<Parametrization*>(1, this), InterpolationDef());
 
 }
 
