30011cb7efd172538a283b2e47be69e7bc6dae3c
Maximilian Sackel <maximilian.sackel@tu-dortmund.de>
2020-03-11 15:45:21 +0000

    merge 'energy_cut_redesign' by hand because of git-lfs problem

--- private/PROPOSAL/EnergyCutSettings.cxx
+++ private/PROPOSAL/EnergyCutSettings.cxx
@@ -1,27 +1,11 @@
+#include "PROPOSAL/EnergyCutSettings.h"
+#include "PROPOSAL/Logging.h"
+#include "PROPOSAL/methods.h"
 
 #include <sstream>
 #include <stdexcept>
-#include "PROPOSAL/EnergyCutSettings.h"
-#include "PROPOSAL/methods.h"
 
 using namespace PROPOSAL;
-
-namespace PROPOSAL {
-
-std::ostream& operator<<(std::ostream& os, EnergyCutSettings const& cut_settings)
-{
-    std::stringstream ss;
-    ss << " EnergyCutSettings (" << &cut_settings << ") ";
-    os << Helper::Centered(60, ss.str()) << '\n';
-
-    os << "Ecut: " << cut_settings.ecut_ << std::endl;
-    os << "Vcut: " << cut_settings.vcut_ << std::endl;
-
-    os << Helper::Centered(60, "");
-    return os;
-}
-
-} // namespace PROPOSAL
 
 //----------------------------------------------------------------------------//
 //-------------------------public member functions----------------------------//
@@ -30,120 +14,86 @@ std::ostream& operator<<(std::ostream& os, EnergyCutSettings const& cut_settings
 double EnergyCutSettings::GetCut(double energy) const
 {
     // if both ecut and vcut are setted (in their bounds), the minimum is used
-    //
-    // if just one of them is setted, this is used
-    //
-    // if both are setted out of their bound (usually -1, -1),
-    // then 1 (which is the max. energy transfer) is returned,
-    // which is always greater than v_max
-    // that means, just continuous losses are calculated, no stochastic ones
-
-    if (ecut_ > 0)
-    {
-        if (vcut_ > 0 && vcut_ <= 1)
-        {
-            return std::min(ecut_ / energy, vcut_);
-        } else
-        {
-            return ecut_ / energy;
-        }
-    } else
-    {
-        if (vcut_ > 0 && vcut_ <= 1)
-        {
-            return vcut_;
-        } else
-        {
-            return 1.;
-        }
-    }
+    return std::min(ecut_, vcut_ * energy);
 }
 
 //----------------------------------------------------------------------------//
 //--------------------------------constructors--------------------------------//
 //----------------------------------------------------------------------------//
 
-EnergyCutSettings::EnergyCutSettings()
-    : ecut_(500.)
-    , vcut_(0.05)
-{
-}
 
-//----------------------------------------------------------------------------//
-//----------------------------------------------------------------------------//
-
-EnergyCutSettings::EnergyCutSettings(const EnergyCutSettings& cuts)
-    : ecut_(cuts.ecut_)
-    , vcut_(cuts.vcut_)
-{
-}
-
-//----------------------------------------------------------------------------//
-//----------------------------------------------------------------------------//
-
-EnergyCutSettings::EnergyCutSettings(const double ecut, const double vcut)
+EnergyCutSettings::EnergyCutSettings(
+    const double ecut, const double vcut, const bool continuous_randomization)
     : ecut_(ecut)
     , vcut_(vcut)
+    , continuous_randomization_(continuous_randomization)
 {
-}
+    if (ecut_ < 0)
+        throw std::invalid_argument(
+            "Absolut EnergyCut (ecut) must be larger > 0! \nINFO: If a ecut = "
+            "std::numeric_limits<double>::infinity() is set the particle will "
+            "only propagated continously. ");
+
+    if (vcut_ <= 0 || vcut_ >= 1)
+        throw std::invalid_argument(
+            "Relative EnergyCut (vcut) must be between [0, 1]. \nINF0: If a vc"
+            "ut = 1 is set, the particle is only propagate continous.");
+
+    log_debug(
+        "EnergyCut set to ecut(%f), vcut(%f), continuous_randomization(%b)",
+        ecut_, vcut_, continuous_randomization_);
+};
 
 EnergyCutSettings::EnergyCutSettings(const nlohmann::json& config)
 {
-    if(not config.is_object()) throw std::invalid_argument("No json object found.");
-    if(not config.at("e_cut").is_number()) throw std::invalid_argument("No e_cut found.");
-    if(not config.at("v_cut").is_number()) throw std::invalid_argument("No v_cut found.");
+    if(!config.contain("e_cut"))
+        throw std::invalid_argument("A e_cut must be defined. e_cut = [0-inf]");
+    if(!config.contain("v_cut"))
+        throw std::invalid_argument("A v_cut must be defined. v_cut = [0-1]");
+    if(!config.contain("cont_rand"))
+        throw std::invalid_argument("cont_rand must be activated/deactivated. cont_rand = true/false");
 
-    config.at("e_cut").get_to(ecut_);
-    config.at("v_cut").get_to(vcut_);
+    double ecut, vcut;
+    bool cont_rand;
+    config.at("e_cut").get_to(ecut);
+    config.at("v_cut").get_to(vcut);
+    config.at("cont_rand").get_to(cont_rand);
 
-   if(not (ecut_ > 0 || ecut_ == -1))
-       throw std::logic_error(
-               "e_cut must be larger than zero or can be disabled by set -1");
-   if(not ((vcut_ >= 0 && vcut_ <= 1) || vcut_ == -1))
-       throw std::logic_error( "v is defined between (0, 1)");
-}
-
-//----------------------------------------------------------------------------//
-//-------------------------operators and swap function------------------------//
-//----------------------------------------------------------------------------//
-
-EnergyCutSettings& EnergyCutSettings::operator=(const EnergyCutSettings& energyCutSettings)
-{
-    if (this != &energyCutSettings)
-    {
-        EnergyCutSettings tmp(energyCutSettings);
-        swap(tmp);
-    }
-    return *this;
-}
+    *this = EnergyCutSettings(ecut, vcut, cont_rand);
+};
 
 //----------------------------------------------------------------------------//
 //----------------------------------------------------------------------------//
 
-bool EnergyCutSettings::operator==(const EnergyCutSettings& energyCutSettings) const
+bool EnergyCutSettings::operator==(
+    const EnergyCutSettings& energyCutSettings) const
 {
     if (ecut_ != energyCutSettings.ecut_)
         return false;
     if (vcut_ != energyCutSettings.vcut_)
         return false;
+    if (continuous_randomization_
+        != energyCutSettings.continuous_randomization_)
+        return false;
 
-    // else
     return true;
 }
 
 //----------------------------------------------------------------------------//
 //----------------------------------------------------------------------------//
 
-bool EnergyCutSettings::operator!=(const EnergyCutSettings& energyCutSettings) const
+std::ostream& PROPOSAL::operator<<(
+    std::ostream& os, PROPOSAL::EnergyCutSettings const& cut_settings)
 {
-    return !(*this == energyCutSettings);
-}
+    std::stringstream ss;
+    ss << " EnergyCutSettings (" << &cut_settings << ") ";
+    os << Helper::Centered(60, ss.str()) << '\n';
 
-//----------------------------------------------------------------------------//
-//----------------------------------------------------------------------------//
+    os << "Ecut: " << cut_settings.ecut_ << std::endl;
+    os << "Vcut: " << cut_settings.vcut_ << std::endl;
+    os << "Continuous randomization: " << cut_settings.continuous_randomization_
+       << std::endl;
 
-void EnergyCutSettings::swap(EnergyCutSettings& energyCutSettings)
-{
-    std::swap(ecut_, energyCutSettings.ecut_);
-    std::swap(vcut_, energyCutSettings.vcut_);
+    os << Helper::Centered(60, "");
+    return os;
 }
--- public/PROPOSAL/EnergyCutSettings.h
+++ public/PROPOSAL/EnergyCutSettings.h
@@ -32,7 +32,6 @@
 #include <iostream>
 #include "PROPOSAL/json.hpp"
 
-
 namespace PROPOSAL {
 
 /**
@@ -52,18 +51,16 @@ class EnergyCutSettings
 private:
     double ecut_;
     double vcut_;
+    bool continuous_randomization_;
 
 public:
     //----------------------------------------------------------------------------//
     // Constructors
 
-    EnergyCutSettings();
-    EnergyCutSettings(const EnergyCutSettings&);
-    EnergyCutSettings& operator=(const EnergyCutSettings& energyCutSettings);
-    EnergyCutSettings(const double ecut, const double vcut);
+    EnergyCutSettings(const double, const double, const bool);
     EnergyCutSettings(const nlohmann::json&);
+
     bool operator==(const EnergyCutSettings& energyCutSettings) const;
-    bool operator!=(const EnergyCutSettings& energyCutSettings) const;
     friend std::ostream& operator<<(std::ostream& os, EnergyCutSettings const& cut_settings);
 
     //----------------------------------------------------------------------------//
@@ -80,21 +77,12 @@ public:
      */
     double GetCut(double energy) const;
 
-    //----------------------------------------------------------------------------//
-
-    void swap(EnergyCutSettings& energyCutSettings);
 
     //----------------------------------------------------------------------------//
     // Getter
 
     double GetEcut() const { return ecut_; }
     double GetVcut() const { return vcut_; }
-
-    //----------------------------------------------------------------------------//
-    // Setter
-
-    void SetEcut(double ecut) { ecut_ = ecut; }
-    void SetVcut(double vcut) { vcut_ = vcut; }
 };
 
 std::ostream& operator<<(std::ostream& os, PROPOSAL::EnergyCutSettings const& cut_settings);
