830827c558e23eade849e984c5b4ebd343ae9a04
sudojan <jan.soedingrekso@tu-dortmund.de>
2020-03-14 18:35:34 +0100

    now with correct paths for version.h in cmake

--- cmake/Standalone_CMakeLists.cmake
+++ cmake/Standalone_CMakeLists.cmake
@@ -93,9 +93,9 @@ target_link_libraries(PROPOSAL log4cplus ${LIBRARIES_TO_LINK})
 
 configure_file(
 	"${PROJECT_SOURCE_DIR}/public/PROPOSAL/version.h.in"
-	"${PROJECT_BINARY_DIR}/public/PROPOSAL/version.h"
+	"${PROJECT_BINARY_DIR}/include/PROPOSAL/version.h"
 )
-install(FILES ${PROJECT_BINARY_DIR}/public/PROPOSAL/version.h DESTINATION include/PROPOSAL)
+install(FILES ${PROJECT_BINARY_DIR}/include/PROPOSAL/version.h DESTINATION include/PROPOSAL)
 target_include_directories(PROPOSAL PUBLIC ${PROJECT_BINARY_DIR}/include)
 
 IF(IS_SYMLINK ${CMAKE_BINARY_DIR}/resources)
