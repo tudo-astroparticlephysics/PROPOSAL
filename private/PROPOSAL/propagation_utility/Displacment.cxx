444b8e59bca4bed2cfdaa4a43b39f4d37cf730b5
Maximilian Sackel <maximilian.sackel@tu-dortmund.de>
2020-03-27 13:41:53 +0000

    add displacment tests and check if at least CrossSection is defined

--- private/PROPOSAL/propagation_utility/Displacment.cxx
+++ private/PROPOSAL/propagation_utility/Displacment.cxx
@@ -3,14 +3,17 @@
 
 using namespace PROPOSAL;
 
-Displacement::Displacement(const CrossSectionList& cross): cross(cross) {
-    if(cross.size() < 1) throw std::invalid_argument("At least one crosssection is required.");
+Displacement::Displacement(const CrossSectionList& cross)
+    : cross(cross)
+{
+    if (cross.size() < 1)
+        throw std::invalid_argument("At least one crosssection is required.");
 }
 
-
-double Displacement::FunctionToIntegral(double energy) {
+double Displacement::FunctionToIntegral(double energy)
+{
     auto result = 0.0;
-    for (const auto& cr: cross)
+    for (const auto& cr : cross)
         result += cr->CalculatedEdx(energy);
 
     return -1.0 / result;
