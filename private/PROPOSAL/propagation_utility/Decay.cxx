7b60854686965dc806676b9d45a5212732bacff4
Maximilian Sackel <maximilian.sackel@tu-dortmund.de>
2020-04-06 14:19:06 +0000

    add decay source

---
+++ private/PROPOSAL/propagation_utility/Decay.cxx
@@ -0,0 +1,6 @@
+#include "PROPOSAL/propagation_utility/Decay.h"
+
+namespace PROPOSAL {
+Interpolant1DBuilder::Definition decay_interpol_def(
+    nullptr, 200, 0., 1e14, 5, false, false, true, 5, false, false, false);
+} // namespace PROPOSAL
