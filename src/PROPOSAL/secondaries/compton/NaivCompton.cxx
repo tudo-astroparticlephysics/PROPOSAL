79b1cb79832233f6be864915aeae44afcee986f2
Maximilian Sackel <maximilian.sackel@gmx.de>
2020-10-16 15:33:14 +0200

    energy conservation in compton scattering

--- src/PROPOSAL/secondaries/compton/NaivCompton.cxx
+++ src/PROPOSAL/secondaries/compton/NaivCompton.cxx
@@ -18,14 +18,14 @@ double secondaries::NaivCompton::CalculateRho(
 }
 
 tuple<Vector3D, Vector3D> secondaries::NaivCompton::CalculateDirections(
-    Vector3D primary_dir, double energy, double rho, double rnd)
+    Vector3D primary_dir, double energy, double v, double rnd)
 {
     auto com_energy = energy + ME; // center of mass energy
     auto kin_energy = energy;      // kinetic energy
-    auto cosphi_gamma = (com_energy * (1. - rho) - ME)
-        / ((1. - rho) * sqrt(com_energy * kin_energy));
+    auto cosphi_gamma = (com_energy * (1. - v) - ME)
+        / ((1. - v) * sqrt(com_energy * kin_energy));
     auto cosphi_electron
-        = (com_energy * rho - ME) / (rho * sqrt(com_energy * kin_energy));
+        = (com_energy * v - ME) / (v * sqrt(com_energy * kin_energy));
     auto rnd_theta = rnd * 2. * PI;
     auto dir_gamma = deflect(primary_dir, cosphi_gamma, rnd_theta);
     auto dir_electron
@@ -34,21 +34,19 @@ tuple<Vector3D, Vector3D> secondaries::NaivCompton::CalculateDirections(
 }
 
 tuple<double, double> secondaries::NaivCompton::CalculateEnergy(
-    double energy, double rho)
+    double energy, double v)
 {
-    auto energy_gamma = energy * (1 - rho);
-    auto energy_electron = energy * rho;
-    return make_tuple(energy_gamma, energy_electron);
+    return make_tuple(energy * (1 - v), energy * v);
 }
 
 vector<Loss::secondary_t> secondaries::NaivCompton::CalculateSecondaries(
     double primary_energy, Loss::secondary_t loss, const Component&,
     vector<double> rnd)
 {
-    auto rho = CalculateRho(primary_energy, get<Loss::ENERGY>(loss));
-    auto secondary_energy = CalculateEnergy(get<Loss::ENERGY>(loss), rho);
+    auto v = get<Loss::ENERGY>(loss) /primary_energy;
+    auto secondary_energy = CalculateEnergy(primary_energy, v);
     auto secondary_dir = CalculateDirections(
-        get<Loss::DIRECTION>(loss), get<Loss::ENERGY>(loss), rho, rnd[1]);
+            get<Loss::DIRECTION>(loss), get<Loss::ENERGY>(loss), v, rnd[1]);
     auto sec = std::vector<Loss::secondary_t>();
     sec.emplace_back(static_cast<int>(ParticleType::Gamma),
         get<Loss::POSITION>(loss), get<GAMMA>(secondary_dir),
