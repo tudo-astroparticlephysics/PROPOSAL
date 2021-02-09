0a2c025e375e68714f654ba1b7c84392b0f22bb0
Maximilian Sackel <maximilian.sackel@gmx.de>
2021-02-09 13:00:23 +0100

    same for dEdx

--- src/PROPOSAL/crosssection/CrossSectionDEDX/AxisBuilderDEDX.cxx
+++ src/PROPOSAL/crosssection/CrossSectionDEDX/AxisBuilderDEDX.cxx
@@ -13,7 +13,7 @@ void AxisBuilderDEDX::refine_definition_range(
     std::function<double(double)> func, unsigned int i)
 {
     auto ax = cubic_splines::ExpAxis<double>(low, up, n);
-    while (not(func(ax.back_transform(i)) > 0) and i < n)
+    while (not(func(ax.back_transform(i+1)) > 0) and i < n)
         ++i;
     if (i == n)
         throw std::logic_error("No positive values to build dEdx tables!");
