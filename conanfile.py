d60009d643dd2cc356ec429df9d43fcaa70eb646
Maximilian Sackel <maximilian.sackel@udo.edu>
2021-03-01 10:18:34 +0100

    update build process for documentation

--- conanfile.py
+++ conanfile.py
@@ -14,12 +14,14 @@ class PROPOSALConan(ConanFile):
         "fPIC": [True, False],
         "build_testing": [True, False],
         "build_python": [True, False],
+        "build_documentation": [True, False],
     }
     default_options = {
         "shared": False,
         "fPIC": True,
         "build_testing": False,
         "build_python": False,
+        "build_documentation": False,
     }
     generators = "cmake"
     _cmake = None
@@ -35,6 +37,8 @@ class PROPOSALConan(ConanFile):
             self.requires("pybind11/2.6.2")
         if self.options.build_testing:
             self.requires("gtest/1.10.0")
+        if self.options.build_documentation:
+            self.requires("doxygen/1.8.20")
 
     def _configure_cmake(self):
         if self._cmake:
@@ -42,6 +46,7 @@ class PROPOSALConan(ConanFile):
         self._cmake = CMake(self)
         self._cmake.definitions["BUILD_TESTING"] = self.options.build_testing
         self._cmake.definitions["BUILD_PYTHON"] = self.options.build_python
+        self._cmake.definitions["BUILD_DOCUMENTATION"] = self.options.build_documentation
         self._cmake.configure()
         return self._cmake
 
