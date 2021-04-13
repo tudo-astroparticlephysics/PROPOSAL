f3392ebb7e617bf830961c40233d94e81ce33bb2
Jean-Marco Alameddine <jean-marco.alameddine@tu-dortmund.de>
2021-04-13 15:13:12 +0200

    Set position independent code cmake variable for pypi

--- setup.py
+++ setup.py
@@ -85,6 +85,7 @@ class build_ext_cmake(build_ext):
             '-DCMAKE_BUILD_TYPE=' + cfg,
             '-DBUILD_TESTING=OFF',
             '-DBUILD_PYTHON=ON',
+            '-DCMAKE_POSITION_INDEPENDENT_CODE=TRUE',
             '-DBUILD_EXAMPLE=OFF',
             '-DPYTHON_EXECUTABLE=' + sys.executable,
             '-DPYTHON_LIBRARY=' + str(python_lib),
