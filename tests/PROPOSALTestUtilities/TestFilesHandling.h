51df5843c8427312edc3f9a82400ae3bd735dc29
Maximilian Sackel <maximilian.sackel@udo.edu>
2021-05-05 10:45:19 +0200

    Use boost filesystem for backwards compability. Now the test should be buildable even on gcc7.
    Delete conanfile.py and substude it through multiple conanfiles providing different usecases.
    If the user want to use an not released version of proposal, he should install it lokal and use a find_package call to avoid ambiguity if the version number has not been updated and faster compile time.

--- tests/PROPOSALTestUtilities/TestFilesHandling.h
+++ tests/PROPOSALTestUtilities/TestFilesHandling.h
@@ -1,18 +1,19 @@
-#include <filesystem>
+#include <boost/filesystem.hpp>
 #include <fstream>
+#include <sstream>
 
 auto getTestFiles(std::string filename)
 {
-    auto directory = std::filesystem::path("tests/TestFiles");
+    auto directory = boost::filesystem::path("tests/TestFiles");
 
     if (auto test_p = std::getenv("PROPOSAL_TEST_FILES"))
-        directory = std::filesystem::path(test_p);
+        directory = boost::filesystem::path(test_p);
 
-    if (std::filesystem::exists(directory / filename))
+    if (boost::filesystem::exists(directory / filename))
         return std::ifstream { directory / filename };
 
     std::ostringstream error;
-    error << "Unable to find TestFile under path: " << directory/filename <<
-    ". Try setting the PROPOSAL_TEST_FILES environment variable.";
-    throw std::invalid_argument( error.str() );
+    error << "Unable to find TestFile under path: " << directory / filename
+          << ". Try setting the PROPOSAL_TEST_FILES environment variable.";
+    throw std::invalid_argument(error.str());
 }
