f3fcac4b2e2e852cd704c192f94db6bb3c97e3eb
Jean-Marco Alameddine <jean-marco.alameddine@tu-dortmund.de>
2022-08-01 11:45:39 +0200

    Add additinal unit test and fix typo

--- tests/Photoeffect_TEST.cxx
+++ tests/Photoeffect_TEST.cxx
@@ -1,5 +1,6 @@
 #include "gtest/gtest.h"
 #include "PROPOSAL/crosssection/parametrization/Photoeffect.h"
+#include "PROPOSAL/crosssection/parametrization/PhotoPairProduction.h"
 #include "PROPOSAL/crosssection/CrossSectionBuilder.h"
 #include "PROPOSAL/medium/Medium.h"
 
@@ -32,11 +33,12 @@ TEST(PhotoeffectSauter, dEdx)
 
     auto cross = make_crosssection(photoeffect, particle, medium, nullptr, false);
 
+    // should be zero for only-stochastic interaction
     EXPECT_EQ(cross->CalculatedEdx(1e3), 0);
     EXPECT_EQ(cross->CalculatedE2dx(1e3), 0);
 }
 
-TEST(PhotoEffectStauer, TestOnlyStochastic)
+TEST(PhotoEffectSauter, TestOnlyStochastic)
 {
     auto particle = GammaDef();
     auto medium = Air();
@@ -45,6 +47,24 @@ TEST(PhotoEffectStauer, TestOnlyStochastic)
 
     // this should be an invalid configuration
     EXPECT_THROW(make_crosssection(photoeffect, particle, medium, cuts, false), std::logic_error);
+}
+
+TEST(PhotoEffectSauter, Comparison)
+{
+    auto particle = GammaDef();
+    auto medium = Air();
+    auto photoeffect = crosssection::PhotoeffectSauter();
+    auto photopair = crosssection::PhotoPairKochMotz();
+
+    auto cross_photoeffect = make_crosssection(photoeffect, particle, medium, nullptr, false);
+    auto cross_photopair = make_crosssection(photopair, particle, medium, nullptr, false);
+
+    // photoeffect should be dominant at low energies, but subdominant at high energies
+    double low_E = 1e0;
+    EXPECT_GT(cross_photoeffect->CalculatedNdx(low_E), cross_photopair->CalculatedNdx(low_E));
+
+    double high_E = 1e6;
+    EXPECT_LT(cross_photoeffect->CalculatedNdx(high_E), cross_photopair->CalculatedNdx(high_E));
 }
 
 int main(int argc, char** argv)
