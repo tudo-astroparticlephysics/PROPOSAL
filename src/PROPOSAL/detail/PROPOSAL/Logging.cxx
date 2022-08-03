53a87e5e0dc7488d8870530b2884cc7e7f9c3715
Jean-Marco Alameddine <jean-marco.alameddine@udo.edu>
2022-08-03 19:55:09 +0200

    set default log level to info

--- src/PROPOSAL/detail/PROPOSAL/Logging.cxx
+++ src/PROPOSAL/detail/PROPOSAL/Logging.cxx
@@ -8,4 +8,4 @@ std::unordered_map<std::string, std::shared_ptr<spdlog::logger>> Logging::logger
 std::shared_ptr<spdlog::sinks::sink> Logging::sink
     = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
 
-spdlog::level::level_enum Logging::global_loglevel = spdlog::level::level_enum::warn;
+spdlog::level::level_enum Logging::global_loglevel = spdlog::level::level_enum::info;
