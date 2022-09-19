0e2b374890382a8e7603dddc529a83081bc8ad0b
Jean-Marco Alameddine <jean-marco.alameddine@udo.edu>
2022-09-19 13:20:49 +0200

    Add additional UnitTests for annihilation

--- tests/Annihilation_TEST.cxx
+++ tests/Annihilation_TEST.cxx
@@ -31,6 +31,22 @@ ParticleDef getParticleDef(const std::string& name)
     }
 }
 
+TEST(Annihilation, Test_of_dEdx)
+{
+    ParticleDef particle_def = EPlusDef();
+    auto medium = Air();
+    nlohmann::json config;
+    config["parametrization"] = "Heitler";
+    auto cross_integral = make_annihilation(particle_def, medium, false, config);
+    auto cross_interpol = make_annihilation(particle_def, medium, false, config);
+
+    EXPECT_EQ(cross_integral->CalculatedEdx(1e3), 0);
+    EXPECT_EQ(cross_integral->CalculatedE2dx(1e3), 0);
+
+    EXPECT_EQ(cross_interpol->CalculatedEdx(1e3), 0);
+    EXPECT_EQ(cross_interpol->CalculatedE2dx(1e3), 0);
+}
+
 TEST(Annihilation, Test_of_dNdx)
 {
     std::ifstream in;
