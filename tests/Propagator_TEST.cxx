03d4646d2af2159ddf556ee7115e7992d0671895
Jean-Marco Alameddine <jean-marco.alameddine@udo.edu>
2023-04-17 20:23:11 +0200

    Change order of enum `Type` so that if `Decay` and `MinimalE` are identical, we use `Decay`. Add UnitTest to check that behaviour described in issue is solved

--- tests/Propagator_TEST.cxx
+++ tests/Propagator_TEST.cxx
@@ -4,6 +4,7 @@
 #include "PROPOSAL/propagation_utility/TimeBuilder.h"
 #include "PROPOSAL/propagation_utility/InteractionBuilder.h"
 #include "PROPOSAL/propagation_utility/ContRandBuilder.h"
+#include "PROPOSAL/propagation_utility/DecayBuilder.h"
 #include "PROPOSAL/density_distr/density_homogeneous.h"
 #include "PROPOSAL/scattering/ScatteringFactory.h"
 #include "PROPOSAL/geometry/Sphere.h"
@@ -56,6 +57,46 @@ TEST(Propagator, min_energy)
             EXPECT_EQ(sec.GetTrackTypes().back(), InteractionType::ContinuousEnergyLoss);
     }
 }
+
+TEST(Propagator, RestMassDecay)
+{
+    // make sure that all muons decay at the end of their propagation, even if they have reached their rest mass
+    // (see issue https://github.com/tudo-astroparticlephysics/PROPOSAL/issues/323)
+    auto p_def = MuMinusDef();
+    auto medium = Ice();
+    auto cuts = std::make_shared<EnergyCutSettings>(INF, 0.05, true);
+    auto cross = GetStdCrossSections(p_def, medium, cuts, true);
+
+    auto collection = PropagationUtility::Collection();
+    collection.interaction_calc = make_interaction(cross, true);
+    collection.displacement_calc = make_displacement(cross, true);
+    collection.time_calc = make_time(cross, p_def, true);
+    collection.cont_rand = make_contrand(cross, true);
+    collection.scattering = make_scattering(MultipleScatteringType::Highland, {}, p_def, medium);
+    collection.decay_calc = make_decay(cross, p_def, true);
+
+    auto prop_utility = PropagationUtility(collection);
+
+    auto density_distr = std::make_shared<Density_homogeneous>(medium);
+    auto world = std::make_shared<Sphere>(Cartesian3D(0, 0, 0), 1e20);
+
+    auto sector = std::make_tuple(world, prop_utility, density_distr);
+    std::vector<Sector> sec_vec = {sector};
+
+    auto prop = Propagator(p_def, sec_vec);
+
+    auto init_state = ParticleState();
+    init_state.energy = 150;
+    init_state.position = Cartesian3D(0, 0, 0);
+    init_state.direction = Cartesian3D(0, 0, 1);
+
+    for (size_t i=0; i<1000; i++) {
+        auto sec = prop.Propagate(init_state);
+        EXPECT_FALSE(sec.GetDecayProducts().empty()); // check that decay products are produced
+    }
+
+}
+
 
 int main(int argc, char** argv)
 {
